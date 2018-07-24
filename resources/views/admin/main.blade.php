25b96bc85c0df8de01f589d43e52afbeb1b99fbe
Lin Yun-Zhong <lzong.tw@gmail.com>
2018-07-25 02:06:08 +0800

    Layout adjustment

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -2,10 +2,8 @@
 <div class="m-content">				          
     <div class="row">
         @include('admin.panel')
-        <div class="content col-lg-9 col-md-8">
-            <div class="">
-                @yield("app-content")
-            </div>
+        <div>
+            @yield("app-content")
         </div>
     </div>
 </div>
