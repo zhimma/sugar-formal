2c000ce0739ca4a5c4e76739686feb64a725318d
Lin Yun-Zhong <lzong.tw@gmail.com>
2018-09-22 03:22:40 +0800

    New function

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -1,5 +1,6 @@
 <div id="mySidenav" class="sidenav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="{{ route('stat/vip') }}">VIP會員統計資料</a>
 	<a href="{{ route('users/manager') }}">會員搜尋(變更男女、VIP資料)</a>
 	<a href="{{ route('users/advSearch') }}">進階會員搜尋</a>
     <a href="{{ route('users/bannedList') }}">會員封鎖清單</a>
