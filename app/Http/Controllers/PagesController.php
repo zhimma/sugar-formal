9465f631c7635b38b2fdad40066aab0b3f46b4d4
LZong <lzong.tw@gmail.com>
2019-01-23 15:54:39 +0800

    Catch VIP cancel failure.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -574,7 +574,15 @@ class PagesController extends Controller
                 $request->session()->save();
                 return redirect('/dashboard')->with('user', $user)->with('message', 'VIP 取消成功！')->with('cancel_notice', '您已成功取消VIP付款，下個月起將不再繼續扣款，目前的VIP權限可以維持到'.$date);
             }
-            return back()->with('message', '帳號密碼輸入錯誤');
+            else{
+                $log = new \App\Models\LogCancelVipFailed();
+                $log->user_id = $user->id;
+                $log->save();
+                return back()->with('message', '帳號密碼輸入錯誤');
+            }
+        }
+        else{
+            Log::error('User not found while canceling VIP.');
         }
     }
 
---
+++ app/Models/LogCancelVipFailed.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogCancelVipFailed extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'log_cancel_vip_failed';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    //protected $fillable = '';
+}
