f605812d076258f57077a24a629939c10066373d
Lin Yun-Zhong <lzong.tw@gmail.com>
2019-02-22 23:43:11 +0800

    Exception Detailed.

--- resources/views/errors/exception.blade.php
+++ resources/views/errors/exception.blade.php
@@ -5,15 +5,28 @@
 <div class="m-portlet__head">
 <div class="m-portlet__head-caption">
     <div class="m-portlet__head-title">
-        <h3 class="m-portlet__head-text">
-        未知的錯誤 <small></small>
-        </h3>
+        @if(isset($exception))
+            <h3 class="m-portlet__head-text">
+            發生錯誤 <small></small>
+            </h3>
+        @else
+            <h3 class="m-portlet__head-text">
+            發生不明錯誤 <small></small>
+            </h3>
+        @endif
     </div>
 </div>
 </div>
 <div class="m-portlet__body">
-<h4>Sorry，網站碰上不明錯誤，請聯絡網站管理員</h4>
-<h5>Line ID : AAABBBCC</h5>
+    @if(isset($exception))
+        <h4>Sorry，網站發生錯誤，請聯絡網站管理員，並提供以下資訊：</h4>
+        <h5>時間：{{ \Carbon\Carbon::now()->toDateTimeString() }}</h5>
+        <h5>錯誤類型：{{ substr($exception, 0, strpos($exception, ':')) }}</h5>
+        <h5>Line ID : AAABBBCC</h5>
+    @else
+        <h4>Sorry，網站發生不明錯誤，請聯絡網站管理員：</h4>
+        <h5>Line ID : AAABBBCC</h5>
+    @endif
 </div>
 
 @stop
