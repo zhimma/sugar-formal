2bad924b780aa0fd74d2ad0b6fe7994fb2fd430b
LZong <lzong.tw@gmail.com>
2019-06-19 20:56:21 +0800

    Female free VIP omments adjusted

--- app/Http/Middleware/FemaleVipActive.php
+++ app/Http/Middleware/FemaleVipActive.php
@@ -52,7 +52,7 @@ class FemaleVipActive
             return $next($request);
         } 
 
-        //如果免費會員(!isVip())失去免費VIP的時間(vip_record)與現在時間的差距小於系統設定的時間長度，則不做任何動作
+        //如果一般會員(!isVip())失去免費VIP的時間(vip_record)與現在時間的差距小於系統設定的時間長度，則不做任何動作
         //這句是為了避免會員失去免費VIP後，馬上就又可以拿到免費VIP而設的條件
         if($vip_record->diffInSeconds(Carbon::now()) <= Config::get('social.vip.start') && !$user->isVip()) {
         }
@@ -62,7 +62,7 @@ class FemaleVipActive
             $user->save();
             Vip::upgrade($user->id, '1111000', '0', 0, 'OOOOOOOO', 1, 1);
         }
-        //如果免費VIP取得權限的時間點與現在時間的差距，小於系統設定的時間長度，則將該時間點延長(延長VIP時間)
+        //如果免費VIP取得權限的時間點與現在時間的差距，小於系統設定的時間長度(代表他固定上線)，則將該時間點延長(延長VIP時間)
         else if($user->isVip() && $vip_record->diffInSeconds(Carbon::now()) <= Config::get('social.vip.free-days')) {
             $user->vip_record = Carbon::now();
             $user->save();
