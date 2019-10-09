eca40851f2773b7e0a58c43d95a3d4266d61c562
LZong <lzong.tw@gmail.com>
2019-10-09 16:48:40 +0800

    EC payment modified.

--- app/Http/Middleware/ApiDataLogger.php
+++ app/Http/Middleware/ApiDataLogger.php
@@ -24,6 +24,12 @@ class ApiDataLogger{
         $this->startTime = microtime(true);
         return $next($request);
     }
+
+    private static function merchantSort($a,$b)
+    {
+        return strcasecmp($a, $b);
+    }
+
     public function terminate($request, $response){
         if ( env('API_DATALOGGER', true) ) {
             if(!str_contains($request->getContent(), 'BankResponseCode')) {
@@ -58,6 +64,71 @@ class ApiDataLogger{
                     Log::info('EC payment failed with user id: ' . $request->CustomField1);
                 }
                 $payload = $request->all();
+                // 變數宣告。
+                $arErrors = array();
+                $arFeedback = array();
+                $szCheckMacValue = '';
+                $EncryptType = 1;
+                // 重新整理回傳參數。
+                foreach ($payload as $keys => $value) {
+                    if ($keys != 'CheckMacValue') {
+                        if ($keys == 'PaymentType') {
+                            $value = str_replace('_CVS', '', $value);
+                            $value = str_replace('_BARCODE', '', $value);
+                            $value = str_replace('_CreditCard', '', $value);
+                        }
+                        if ($keys == 'PeriodType') {
+                            $value = str_replace('Y', 'Year', $value);
+                            $value = str_replace('M', 'Month', $value);
+                            $value = str_replace('D', 'Day', $value);
+                        }
+                        $arFeedback[$keys] = $value;
+                    }
+                }
+
+                $payloadCheckMacValue = $payload['CheckMacValue'];
+                unset($payload['CheckMacValue']);
+                uksort($payload, array('\App\Http\Middleware\ApiDataLogger','merchantSort'));
+
+                // 組合字串
+                $sMacValue = 'HashKey=' . config('ecpay.payment.HashKey') ;
+                foreach($payload as $key => $value)
+                {
+                    $sMacValue .= '&' . $key . '=' . $value ;
+                }
+
+                $sMacValue .= '&HashIV=' . config('ecpay.payment.HashIV') ;
+
+                // URL Encode編碼
+                $sMacValue = urlencode($sMacValue);
+
+                // 轉成小寫
+                $sMacValue = strtolower($sMacValue);
+
+                // 取代為與 dotNet 相符的字元
+                $sMacValue = str_replace('%2d', '-', $sMacValue);
+                $sMacValue = str_replace('%5f', '_', $sMacValue);
+                $sMacValue = str_replace('%2e', '.', $sMacValue);
+                $sMacValue = str_replace('%21', '!', $sMacValue);
+                $sMacValue = str_replace('%2a', '*', $sMacValue);
+                $sMacValue = str_replace('%28', '(', $sMacValue);
+                $sMacValue = str_replace('%29', ')', $sMacValue);
+
+                // 編碼
+                $sMacValue = hash('sha256', $sMacValue);
+
+                $CheckMacValue = strtoupper($sMacValue);
+
+                if ($CheckMacValue != $payloadCheckMacValue) {
+                    Log::info('CheckMacValue verify fail.');
+                    return '0|Error';
+                }
+
+                if (sizeof($arErrors) > 0) {
+                    Log::info($arErrors);
+                    return '0|Error';
+                }
+
                 $pool = '';
                 $count = 0;
                 foreach ($payload as $key => $value){
@@ -85,7 +156,7 @@ class ApiDataLogger{
                         $this->logService->upgradeLogEC($payload, $user->id);
                         $this->logService->writeLogToDB();
                         $this->logService->writeLogToFile();
-                        Vip::upgrade($user->id, $payload['MerchantID'], $payload['MerchantTradeNo'], $payload['TradeAmt'], $payload['CheckMacValue'], 1, 0);
+                        Vip::upgrade($user->id, $payload['MerchantID'], $payload['MerchantTradeNo'], $payload['TradeAmt'], '', 1, 0);
                         return '1|OK';
                     }
                     else{
