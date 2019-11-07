3fd71ebd7295fc379622c61f520313d481b132e2
henry <vorlino79@gmail.com>
2019-11-08 00:29:18 +0800

    新切版

---
+++ public/js/mem/main.js
@@ -0,0 +1,30 @@
+
+var bottomImg = ''
+var bottomTxt = '\n' +
+    '   >'
+
+$(function () {
+    $('#menuButton').click(function (){ // 菜单栏
+        if ($('#menuList').hasClass('ulHeight')) {
+            $(this).find('img').attr({'src': 'images/icon.png'})
+            $('#menuList').removeClass('ulHeight')
+            $('.menuBg').hide()
+        } else {
+            $(this).find('img').attr({'src': 'images/menu.png'})
+            $('#menuList').addClass('ulHeight')
+            $('.menuBg').fadeIn()
+        }
+    })
+    $('.menuBg').click(function () {
+        $('#menuList').removeClass('ulHeight')
+        $(this).hide()
+    })
+    $('#bottomNav').html(bottomImg + bottomTxt)
+
+    $('.productMain').find('dl').find('dd').click(function () { // 切换产品页
+        $('.productMain').find('dl').find('dd').find('img').removeClass('imgThis')
+        $(this).find('img').addClass('imgThis')
+        var thisNum = $(this).index()
+        $('.productList01').eq(thisNum).fadeIn().siblings('.productList01').hide()
+    })
+})
