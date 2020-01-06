dd4185be0f335b90a239506ef3085b50c1f9b107
LZong <lzong.tw@gmail.com>
2020-01-06 20:33:54 +0800

    Bug fixed.

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -832,7 +832,7 @@ class UserController extends Controller
 
 
             $message_msg = Message::where('to_id', $to_user->id)->where('from_id',$user->id)->get();   
-        if(!$msglib_report->isEmpty()){
+        if(!$msglib_report->isEmpty() && isset($message_msg)){
             foreach($msglib_report as $key=>$msg){
                 $msglib_msg[$key] = str_replace('|$report|',$user->name, $msg['msg']);
                 $msglib_msg[$key] = str_replace('|$reported|',$to_user->name, $msglib_msg[$key]);
@@ -844,7 +844,7 @@ class UserController extends Controller
                 $msglib_msg[$key] = $msg['msg'];
             }
         }
-        if(!$msglib_reported->isEmpty()){
+        if(!$msglib_reported->isEmpty() && isset($message_msg)){
             foreach($msglib_reported as $key=>$msg){
                 $msglib_msg2[$key] = str_replace('|$report|',$user->name, $msg['msg']);
                 $msglib_msg2[$key] = str_replace('|$reported|',$to_user->name, $msglib_msg2[$key]);
