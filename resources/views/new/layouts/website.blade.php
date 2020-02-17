92f06ed0f432382062b58507df1310f105e19f9d
LZong <lzong.tw@gmail.com>
2020-02-18 00:59:56 +0800

    Wrong merge modified.

--- resources/views/new/layouts/website.blade.php
+++ resources/views/new/layouts/website.blade.php
@@ -30,15 +30,9 @@
                         <div class="ggnr01 ">
                         <div class="gg_nr">{!! nl2br($a->content) !!}</div>
                         <div class="gg_bg">
-                                <a href="javascript:void(0);" class="gg_page"><img src="/new/images/bk_03.png"></a>
-                                <a class="ggbut" href="" onclick="disableAnnounce( {{ $a->id }} )" style="bottom: 10px;">不要顯示本公告</a>
-                                <a href="javascript:void(0);" class="gg_pager"><img src="/new/images/bk_05.png" ></a>
-                                </div>
-                            
-                            <div class="gg_bg">
-                                
-                                <a class="ggbut" href="" onclick="disableAnnounce( {{ $a->id }} )" style="bottom: 10px;">不要顯示本公告</a>
-                                
+                            <a href="javascript:void(0);" class="gg_page"><img src="/new/images/bk_03.png"></a>
+                            <a class="ggbut" href="" onclick="disableAnnounce( {{ $a->id }} )" style="bottom: 10px;">不要顯示本公告</a>
+                            <a href="javascript:void(0);" class="gg_pager"><img src="/new/images/bk_05.png" ></a>
                             </div>
                         </div>
                         <a id="" onclick="gmBtn1()" class="bl_gb"><img src="/new/images/gb_icon01.png" style="width: 30px;"></a>
