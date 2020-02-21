45afff39d5acb4487cfe3dec4a4dd34666937996
LZong <lzong.tw@gmail.com>
2020-02-21 15:46:47 +0800

    Contact information modified.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,9 +12,9 @@
 </div>
 </div>
 <div class="m-portlet__body">
-    <li><h4>站長line：@kti1557f(包含@哦)</h4></li>
+    <li><h4>站長line：@giv4956r (包含@哦)</h4></li>
     <li><h4>站長email：admin@sugar-garden.org<h4></li>
-    <li><h4>網站問題回報：@kti1557f<h4></li>
+    <li><h4>網站問題回報：@giv4956r<h4></li>
     <li><h4>網站問題回報：admin@sugar-garden.org<h4></li>
 </div>
 
