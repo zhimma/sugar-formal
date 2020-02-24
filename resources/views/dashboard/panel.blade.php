48581b4780b1e579e65191c3272caf433a8f2156
LZong <lzong.tw@gmail.com>
2020-02-24 15:10:21 +0800

    Production codes synchronized, link to old layouts.

--- resources/views/dashboard/panel.blade.php
+++ resources/views/dashboard/panel.blade.php
@@ -236,6 +236,33 @@ if(Auth::user()) $login_user = Auth::user();
         <button type="submit" class="btn btn-danger">送出</button>
 
 		<button type="button" class="btn btn-outline-danger" data-dismiss="modal">取消</button>
+	</form>
+  </div>
+</div>
+</div>
+</div>
+
+<div class="modal fade" id="m_modal_2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+  <div class="modal-header">
+	<h5 class="modal-title" id="exampleModalLabel">車馬費信息給 @if(isset($cur)) {{ $cur->name }} @elseif(isset($to)) {{ $to->name }} @endif</h5>
+	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	  <span aria-hidden="true">&times;</span>
+	</button>
+  </div>
+  <div class="modal-body">
+  <form method="POST" action="/dashboard/chatpaycomment">
+  <input type="hidden" name="_token" value="{{ csrf_token() }}" >
+	<input type="hidden" name="userId" value="{{$user->id}}">
+	<input type="hidden" name="to" value="@if(isset($cur)){{$cur->id}} @elseif(isset($to)){{$to->id}}@endif">
+		   <textarea class="form-control m-input" name="msg" id="msg" rows="4"></textarea>
+  </div>
+  <div class="modal-footer">
+
+	<button type="submit" class="btn btn-danger">送出</button>
+
+	<button type="button" class="btn btn-outline-danger" data-dismiss="modal">取消</button>
 		</form>
       </div>
     </div>
