3702a85b8d22d49c9e26fa3eea73b16ec865643f
henry <henry>
2020-02-25 20:55:37 +0800

    posts adjust

--- resources/views/dashboard/posts.blade.php
+++ resources/views/dashboard/posts.blade.php
@@ -52,6 +52,37 @@
 	</head>
 
 	<body>
+	<div class="head_3 head hetop">
+		<div class="container">
+			<div class="col-sm-12 col-xs-12 col-md-12">
+				<a href="{!! url('') !!}" >
+					<img src="/new/images/icon_41.png" class="logo" />
+				</a>
+				@if (Auth::user() && Request::path() != '/activate' && Request::path() != '/activate/send-token')
+					@if(Session::has('original_user'))
+						<div class="ndlrfont">
+							<a href="{{ route('escape') }}" class="m-nav__link m-dropdown__toggle">
+								回到原使用者
+							</a></div>
+					@endif
+					@if(!str_contains(url()->current(), 'dashboard') && !str_contains(url()->current(), 'contact') && !str_contains(url()->current(), 'notification') && !str_contains(url()->current(), 'feature') && !str_contains(url()->current(), 'terms') && !str_contains(url()->current(), 'activate') && Auth::user() /*&& Request::path() != '/activate' && Request::path() != '/activate/send-token'*/)
+					<div class="ndlrfont">
+						<a href="{!! url('dashboard/search') !!}"><img src="/new/images/icon_38.png"></a>
+						<span class="getNum">
+							<a href="{!! url('dashboard/chat2/'.csrf_token().\Carbon\Carbon::now()->timestamp) !!}">
+								<img src="/new/images/icon_45.png">
+							</a>
+							<span>{{ \App\Models\Message::unread($user->id) }}</span>
+						</span>
+						<a href="{!! url('dashboard') !!}"><img src="/new/images/icon_48.png"></a>
+					</div>
+					@endif
+				@else
+					<div class="ndlrfont"><a href="{!! url('/checkAdult') !!}">註冊</a>丨<a href="{!! url('login') !!}">登入</a></div>
+				@endif
+			</div>
+		</div>
+	</div>
 	<div class="head heicon">
 			<div class="container">
 				<div class="col-sm-12 col-xs-12 col-md-12">
