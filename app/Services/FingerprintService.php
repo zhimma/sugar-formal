87b236b6fb0ad17930b850d35db4398d3e90a58a
LZong <lzong.tw@gmail.com>
2020-03-17 00:56:30 +0800

    Implicitly banning users function added - undone.

--- app/Services/FingerprintService.php
+++ app/Services/FingerprintService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Fingerprint;
+use App\Models\Fingerprint2;
 use App\Models\User;
 
 class FingerprintService{
@@ -19,7 +20,54 @@ class FingerprintService{
     	$result = Fingerprint::where($fingerprint)->count();
         return $result > 0 ? true : false;
     }
-    
+
+    public function judgeUserFingerprintAll($userId, $fingerprint){
+        if(isset($fingerprint['audio'])){ unset($fingerprint['audio']); }
+        if(isset($fingerprint['created_at'])){ unset($fingerprint['created_at']); }
+        if(isset($fingerprint['batterylevel'])){ unset($fingerprint['batterylevel']); }
+        if(isset($fingerprint['enumerateDevices'])){ unset($fingerprint['enumerateDevices']); }
+        if(isset($fingerprint['ip'])){ unset($fingerprint['ip']); }
+        if(isset($fingerprint['fp'])){ unset($fingerprint['fp']); }
+        if(isset($fingerprint['pixelRatio'])){ unset($fingerprint['pixelRatio']); }
+        if(isset($fingerprint['userAgent'])){ unset($fingerprint['userAgent']); }
+        if(isset($fingerprint['_token'])){ unset($fingerprint['_token']); }
+        if(isset($fingerprint['user_id'])){ unset($fingerprint['user_id']); }
+        if(isset($fingerprint['email'])){ unset($fingerprint['email']); }
+        if(isset($fingerprint['password'])){ unset($fingerprint['password']); }
+        $result = Fingerprint2::where($fingerprint)->get();
+        $final_result = array();
+        foreach($result as $r){
+            if($r->user_id != $userId){
+                array_push($final_result, $r);
+            }
+        }
+        if(count($final_result) > 0){
+            $ids = array_map(function ($array) { return $array->user_id; }, $final_result);
+            \DB::table('banned_users_implicitly')->insert(
+                ['user_id' => $userId,
+                 'target' => implode(", ", $ids),
+                'created_at' => \Carbon\Carbon::now()]
+            );
+        }
+    }
+
+    public function judgeUserFingerprintCanvasOnly($userId, $fingerprint){
+        $result = Fingerprint2::where('canvas', $fingerprint['canvas'])->get();
+        $final_result = array();
+        foreach($result as $r){
+            if($r->user_id != $userId){
+                array_push($final_result, $r);
+            }
+        }
+        if(count($final_result) > 0){
+            $ids = array_map(function ($array) { return $array->user_id; }, $final_result);
+            \DB::table('warning_users')->insert(
+                ['user_id' => $userId,
+                'target' => implode(", ", $ids),
+                'created_at' => \Carbon\Carbon::now()]
+            );
+        }
+    }
 }
 
 ?>
