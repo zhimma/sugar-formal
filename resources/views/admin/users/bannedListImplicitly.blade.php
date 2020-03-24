b50a3da55008379622593f8610163b041423d783
LZong <lzong.tw@gmail.com>
2020-03-24 13:46:56 +0800

    Banned user implicitly modified.

--- resources/views/admin/users/bannedListImplicitly.blade.php
+++ resources/views/admin/users/bannedListImplicitly.blade.php
@@ -7,37 +7,47 @@
 </style>
 <body style="padding: 15px;">
 <h1>隱性封鎖清單</h1>
-共 {{ $users->total() }} 筆資料
+共 {{ $users->count() }} 筆資料
 <table class='table table-bordered table-hover'>
 	<tr>
-        <td>永久封鎖會員 ID</td>
-		<td>永久封鎖會員 Email(暱稱)</td>
-		<td>最新上站時間</td>
-        <td>近三天發文數</td>
-        <td>比對目標</td>
-		<td>封鎖日期</td>
+        <td>Hash 值</td>
+		<td>Email</td>
+		<td>封鎖方式</td>
+        <td>封鎖日期</td>
+        <td>帳號建立時間</td>
+        <td>最近上站時間</td>
+        <td>暱稱</td>
+        <td>標題</td>
+        <td>被檢舉次數</td>
 	</tr>
 	@forelse($users as $user)
-    <tr>
-        <td>{{ $user->user_id }}</td>
-        <td><a href="advInfo/{{ $user->user_id }}" target="_blank">{{ $user->email }}({{ $user->name }})</a></td>
-        <td>{{ $user->last_login }}</td>
-        <td>{{ $user->count }}</td>
-        <td>
-            @if(isset($user->target))
-                <a href="advInfo/{{ $user->target->id }}" target="_blank">{{ $user->target->name }}</a>
-            @else
-                {{ logger('Empty data found in warningList, user id: ' . $user->user_id) }}
-            @endif
-        </td>
-        <td>{{ $user->created_at }}</td>
-    </tr>
+        @php
+            $user->data = User::findById($r->user_id);
+            $user->count = $this->admin->countReported($r->user_id);
+        @endphp
+        @if(isset($user->data))
+            <tr>
+                <td>{{ isset($user->fp) ? $user->fp->fp : '無資料' }}</td>
+                <td><a href="advInfo/{{ $user->data->user_id }}" target="_blank">{{ $user->data->email }}</a></td>
+                <td>{{ $user->type }}</td>
+                <td>{{ $user->banned_at }}</td>
+                <td>{{ $user->data->created_at }}</td>
+                <td>{{ $user->data->last_login }}</td>
+                <td>{{ $user->data->name }}</td>
+                <td>{{ $user->data->title }}</td>
+                <td>{{ $user->count }}</td>
+            </tr>
+        @else
+            <tr>
+                <td colspan="9">無會員資料</td>
+            </tr>
+        @endif
     @empty
     <tr>
         找不到資料
     </tr>
     @endforelse
 </table>
-{!! $users->links() !!}
+{!! $users->render() !!}
 </body>
 @stop
