38a129c3958e0e659d81ee5a3dfdafc2caffa030
LZong <lzong.tw@gmail.com>
2020-08-14 17:48:30 +0800

    VIP if payment failed then do auto cancellation.

--- app/Jobs/CheckECpay.php
+++ app/Jobs/CheckECpay.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Log;
+use App\Models\User;
 
 class CheckECpay implements ShouldQueue
 {
@@ -67,6 +68,14 @@ class CheckECpay implements ShouldQueue
             if($last['RtnCode'] == 1 && $lastProcessDate->diffInDays($now) > 31){
                 Log::info('付費失敗');
                 Log::info($paymentData);
+
+                $admin = User::findByEmail(Config::get('social.admin.email'));
+                $user = User::findById($this->vipData->member_id);
+                $user->getVipData(true)->removeVIP();
+                \App\Models\VipLog::addToLog($user->id, 'Auto cancel', '自動取消', 0, 0);
+                $message = $user->name . "您好，您的 VIP 付費(卡號後四碼 " . $paymentData['card4no'] . ")最後一次付費月份為 " . $lastProcessDate->format('Y 年 m 月') . " ，距今已逾一個月，故停止您的 VIP 權限。優選會員資格一併取消，若有疑問請點右下聯絡我們連絡站長。";
+                \App\Models\Message_new::post($admin->id, $user->id, $message);
+
                 $str = '末四碼：' . $paymentData['card4no'] . "<br>" .
                     "會員 ID：" . $this->vipData->member_id . "<br>" .
                     "訂單編號：" . $this->vipData->order_id;
@@ -79,6 +88,14 @@ class CheckECpay implements ShouldQueue
             else if($last['RtnCode'] == 0){
                 Log::info('付費失敗');
                 Log::info($paymentData);
+
+                $admin = User::findByEmail(Config::get('social.admin.email'));
+                $user = User::findById($this->vipData->member_id);
+                $user->getVipData(true)->removeVIP();
+                \App\Models\VipLog::addToLog($user->id, 'Auto cancel', '自動取消', 0, 0);
+                $message = $user->name . "您好，您的 VIP 付費(卡號後四碼 " . $paymentData['card4no'] . ")已於 " . $lastProcessDate->format('Y 年 m 月') . " 扣款失敗，故停止您的 VIP 權限。優選會員資格一併取消，若有疑問請點右下聯絡我們連絡站長。";
+                \App\Models\Message_new::post($admin->id, $user->id, $message);
+
                 $str = '末四碼：' . $paymentData['card4no'] . "<br>" .
                     "會員 ID：" . $this->vipData->member_id . "<br>" .
                     "訂單編號：" . $this->vipData->order_id;
