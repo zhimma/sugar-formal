f73c6a9441418653b172a6b3076ccc88d1b97da1
LZong <lzong.tw@gmail.com>
2020-10-15 22:51:24 +0800

    Codes modified.

--- config/ecpay.php
+++ config/ecpay.php
@@ -15,15 +15,14 @@ return [
         'uid' => "53538851",  //統一編號
         'HashKey' => "5294y06JbISpM5x9",
         'HashIV' => "v77hoKGq4kWxNNIS",
-        //'ActionURL' => "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址
-        'ActionURL' => "https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址(測試用)
-        'ReturnURL' => "https://linna.test-tw.icu/dashboard/upgradepayEC",  //背景傳送付款結果的網址
-        'postChatpayReturnURL' => "https://linna.test-tw.icu/dashboard/postChatpayEC",  //背景傳送車馬費付款結果的網址
-        'PeriodReturnURL' => "https://linna.test-tw.icu/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
-        'ClientChatpayBackURL' => "https://linna.test-tw.icu/dashboard/chat2", //返回商店的網址
-        'ClientBackURL' => "https://linna.test-tw.icu/dashboard", //返回商店的網址
-        'ServiceURL' => "https://payment-stage.ecpay.com.tw/Cashier/QueryCreditCardPeriodInfo", //定期定額查詢訂單(測試用)
-        'PaymentInfoURL' => "https://linna.test-tw.icu/dashboard/paymentInfoEC",
+        'ActionURL' => "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5",  //送出訂單的網址
+        'ReturnURL' => "https://www.sugar-garden.org/dashboard/upgradepayEC",  //背景傳送付款結果的網址        
+        'postChatpayReturnURL' => "https://www.sugar-garden.org/dashboard/postChatpayEC",  //背景傳送車馬費付款結果的網址
+        'PeriodReturnURL' => "https://www.sugar-garden.org/dashboard/upgradepayEC",  //背景傳送定期定額付款交易結果的網址
+        'ClientChatpayBackURL' => "https:///www.sugar-garden.org/dashboard/chat2", //返回商店的網址
+        'ClientBackURL' => "https://www.sugar-garden.org/dashboard", //返回商店的網址
+        'ServiceURL' => "https://payment.ecpay.com.tw/Cashier/QueryCreditCardPeriodInfo", //定期定額查詢訂單(測試用)
+        'PaymentInfoURL' => "https://www.sugar-garden.org/dashboard/paymentInfoEC",
         // 'OrderResultURL' => "http://www.sugar-garden.org/dashboard/upgradepay",  //付款結果的網址，若不設則會使用綠界的付款結果
     ],
     'payment_test' => [
