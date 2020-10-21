c0db11186d64a29046f9981595844cdac7a6b7fb
yue <yueitng1214@gmail.com>
2020-10-21 09:29:11 +0800

    modify account validator

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -324,10 +324,7 @@ class LoginController extends Controller
 
             //重新驗證帳號密碼
             if (\Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-                $this->login($request);
-            }else{
-                $this->incrementLoginAttempts($request);
-                return $this->sendFailedLoginResponse($request);
+                return redirect('/dashboard');
             }
         }
     }
