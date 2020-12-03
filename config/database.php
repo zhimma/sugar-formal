c5039658f5d26c2a91797c649ab55a639fabcf74
LZong <lzong.tw@gmail.com>
2020-12-03 23:05:59 +0800

    Database fallback values.

--- config/database.php
+++ config/database.php
@@ -38,14 +38,14 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
-
+        
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'webhelper_sugar'),
-            'username' => env('DB_USERNAME', 'webhelper_sugar'),
-            'password' => env('DB_PASSWORD', 'webhelper_sugar'),
+            'database' => env('DB_DATABASE', 'sugar_garden'),
+            'username' => env('DB_USERNAME', 'sguser'),
+            'password' => env('DB_PASSWORD', 'hC`Yz~`?k2N]dIem65t'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
