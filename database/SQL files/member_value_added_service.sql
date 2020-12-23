c08974019db9c3acc149e167a24cd4ec62f69122
Linna <ellelinna@gmail.com>
2020-12-23 11:32:55 +0800

    Commit for 'e6bd185'

---
+++ database/SQL files/member_value_added_service.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 
+-- 伺服器版本： 10.4.8-MariaDB
+-- PHP 版本： 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `sguser_sugarg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `member_value_added_service`
+--
+
+CREATE TABLE `member_value_added_service` (
+  `id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `service_name` varchar(60) NOT NULL,
+  `business_id` varchar(50) NOT NULL,
+  `order_id` varchar(50) NOT NULL,
+  `txn_id` varchar(100) DEFAULT NULL,
+  `amount` int(10) UNSIGNED NOT NULL,
+  `expiry` datetime NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `payment` varchar(60) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `member_value_added_service`
+--
+ALTER TABLE `member_value_added_service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `member_value_added_service`
+--
+ALTER TABLE `member_value_added_service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
