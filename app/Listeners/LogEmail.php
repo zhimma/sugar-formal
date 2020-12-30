73b2dd459ebbc609524b3b33c418c00c4d6b04e2
LZong <lzong.tw@gmail.com>
2020-12-30 16:51:14 +0800

    Bug fixed.

--- app/Listeners/LogEmail.php
+++ app/Listeners/LogEmail.php
@@ -9,6 +9,6 @@ class LogEmail{
         $message = $event->message;
 
         // The Swift_Message has a __toString method so you should be able to log it.
-        Log::info($message);
+        \Log::info($message);
     }
 }
