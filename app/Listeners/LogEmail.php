7429f0440ae17e19bbd739c70253338b6124dde2
sai2207613@gmail.com <admin@li1605-102.members.linode.com>
2020-12-30 09:06:25 +0000

    Log modified.

--- app/Listeners/LogEmail.php
+++ app/Listeners/LogEmail.php
@@ -9,6 +9,8 @@ class LogEmail{
         $message = $event->message;
 
         // The Swift_Message has a __toString method so you should be able to log it.
-        \Log::info($message);
+	$arr = explode("#", $message, 2);
+	$first = $arr[0];
+        \Log::info($first);
     }
 }
