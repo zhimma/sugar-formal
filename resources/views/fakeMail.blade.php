686981e0323d4e8a7421b5ca6209c1f986f1e7f0
LZong <lzong.tw@gmail.com>
2021-01-22 23:26:26 +0800

    Fast fake mail.

---
+++ resources/views/fakeMail.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.website')
+
+@section('app-content')
+
+<div class="m-portlet__head">
+    <div class="m-portlet__head-caption">
+        <div class="m-portlet__head-title">
+            <h3 class="m-portlet__head-text">
+            Fake Mail <small></small>
+            </h3>
+        </div>
+    </div>
+</div>
+<div class="m-portlet__body">
+    Mail Body
+</div>
+
+@stop
