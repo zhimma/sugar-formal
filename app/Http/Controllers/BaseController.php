9116cadbf2aef6a0248ea481772af9fdff641072
LZong <lzong.tw@gmail.com>
2021-02-11 12:06:55 +0800

    Chat performance modified.

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -10,17 +10,19 @@ class BaseController extends Controller
     protected $user, $user_meta, $userIsVip, $unread, $valueAddedServices;
 
     public function __construct() {
-        $this->middleware('global');
-        $this->middleware(function ($request, $next) {
-            $this->user = view()->shared('user');
-            $this->user_meta = view()->shared('user_meta');
-            $this->userIsVip = view()->shared('isVip');
-            $this->userIsFreeVip = view()->shared('isFreeVip');
-            $this->userVipData = view()->shared('vipData');
-            $this->unread = view()->shared('unread');
-            $this->valueAddedServices = view()->shared('valueAddedServices');
+        if(\Request::route()->getName() != "showMessages"){
+            $this->middleware('global');
+            $this->middleware(function ($request, $next) {
+                $this->user = view()->shared('user');
+                $this->user_meta = view()->shared('user_meta');
+                $this->userIsVip = view()->shared('isVip');
+                $this->userIsFreeVip = view()->shared('isFreeVip');
+                $this->userVipData = view()->shared('vipData');
+                $this->unread = view()->shared('unread');
+                $this->valueAddedServices = view()->shared('valueAddedServices');
 
-            return $next($request);
-        });
+                return $next($request);
+            });
+        }
     }
 }
