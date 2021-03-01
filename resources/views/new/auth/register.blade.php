7429b15afb41aa3f9f982256764a9dcfb6c05970
s98113169 <s98113169@gmail.com>
2021-03-01 23:33:38 +0800

    註冊時沒有填寫的欄位，由上往下依序明確提示

--- resources/views/new/auth/register.blade.php
+++ resources/views/new/auth/register.blade.php
@@ -15,27 +15,27 @@
                         {!! csrf_field() !!}
                         <div class="de_input01 dlmarbot">
                             <input type="password" style="display:none" />
-                            <input required name="email" type="email" class="zcinput" placeholder="E-mail 信箱（也是您未來的帳號）" value="{{old('email')}}" autocomplete="off">
+                            <input data-parsley-required name="email" type="email" class="zcinput" placeholder="E-mail 信箱（也是您未來的帳號）" value="{{old('email')}}" autocomplete="off" data-parsley-required-message="請輸入Email">
                         </div>
                         <div class="de_input01 dlmarbot">
                             <input type="password" style="display:none" />
-                            <input style="-webkit-text-security: disc;" required id="pw" minlength="6"  data-parsley-minlength="6" name="password" type="password" class="zcinput" placeholder="密碼" value="{{old('password')}}" autocomplete="off">
+                            <input style="-webkit-text-security: disc;" data-parsley-required id="pw" minlength="6"  data-parsley-minlength="6" name="password" type="password" class="zcinput" placeholder="密碼" value="{{old('password')}}" autocomplete="off" data-parsley-required-message="請輸入密碼">
                         </div>
                         <div class="de_input01 dlmarbot">
-                            <input  data-parsley-equalto="#pw" required name="password_confirmation" type="password" class="zcinput" placeholder="密碼確認" value="{{old('password_confirmation')}}" autocomplete="off">
+                            <input  data-parsley-equalto="#pw" data-parsley-required name="password_confirmation" type="password" class="zcinput" placeholder="密碼確認" value="{{old('password_confirmation')}}" autocomplete="off" data-parsley-required-message="請再次輸入密碼">
                         </div>
                         <div class="de_input01 dlmarbot">
-                            <input required id="name" name="name"  type="text" class="zcinput" placeholder="暱稱 (至多八個字)" value="{{old('name')}}" maxlength="8">
+                            <input data-parsley-required id="name" name="name"  type="text" class="zcinput" placeholder="暱稱 (至多八個字)" value="{{old('name')}}" maxlength="8" data-parsley-required-message="請輸入暱稱">
                         </div>
                         <div class="de_input01 dlmarbot">
-                            <input required name="title" type="text" class="zcinput" placeholder="一句話形容自已"  value="{{old('title')}}">
+                            <input data-parsley-required name="title" type="text" class="zcinput" placeholder="一句話形容自已"  value="{{old('title')}}" data-parsley-required-message="請輸入一句話形容自己">
                         </div>
 
                         <div class="de_input02">
                             <h2>帳號類型（Daddy / Baby）</h2>
-                            <h3><input required type="radio"  name="engroup" value="1" id="RadioGroup1_0" {{old('engroup') == '1'? 'checked' : '' }}><span>甜心大哥</span></h3>
+                            <h3><input data-parsley-required data-parsley-required-message="請選擇帳號類型" type="radio"  name="engroup" value="1" id="RadioGroup1_0" {{old('engroup') == '1'? 'checked' : '' }}><span>甜心大哥</span></h3>
                             <h4>你願意用禮物、美食、旅遊等種種方式，寵愛對方，為了得到他的陪伴</h4>
-                            <h3><input required type="radio" name="engroup" value="2" id="RadioGroup1_1" {{old('engroup') == '2'? 'checked' : '' }}><span>甜心寶貝</span></h3>
+                            <h3><input data-parsley-required data-parsley-required-message="請選擇帳號類型" type="radio" name="engroup" value="2" id="RadioGroup1_1" {{old('engroup') == '2'? 'checked' : '' }}><span>甜心寶貝</span></h3>
                             <h4>妳想得到寵愛，陪伴甜心大哥</h4>
                         </div>
                         <br>
@@ -49,7 +49,7 @@
                             @endforeach
                         </div>
 
-                        <div class="decheck"><input required name="agree" type="checkbox" {{old('agree') == 'on'? 'checked' : '' }}><span>我同意甜心花園的使用條款和隱私政策</span></div>
+                        <div class="decheck"><input data-parsley-required data-parsley-required-message="請勾選同意使用條款和隱私政策" name="agree" type="checkbox" {{old('agree') == 'on'? 'checked' : '' }}><span>我同意甜心花園的使用條款和隱私政策</span></div>
                         <input type="hidden" name="google_recaptcha_token" id="ctl-recaptcha-token">
                         {{-- <a href="javascript:void(0);" onclick="this.disabled = true" class="dlbut btn-register">註冊</a> --}}
                         <button onclick="this.disabled = true" class="dlbut btn-register" style="border-style: none;">註冊</button>
@@ -87,6 +87,7 @@
                 var msg=[];
                 for (var i = 0; i <error.length; i++) {
                     msg[i]=error.eq(i).html();
+                    break;
                 }
                 msg = Array.from(new Set(msg));
                 // ResultData({
