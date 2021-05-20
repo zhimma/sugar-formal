674faee56745bf5d49b61724ddc09a70322eae9e
LZong <lzong.tw@gmail.com>
2021-05-20 19:41:39 +0800

    Exception handler modified.

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -7,11 +7,11 @@
             <div class="col-sm-12 col-xs-12 col-md-12">
                 <div class="wxsy" style="min-height: 0!important;">
                     <div class="wxsy_title">
-                        連線過多
+                        連線頻率過高
                     </div>
                     <div class="wxsy_k">
                         <div class="wknr">
-                            <h5>您目前連線次數過多，請稍後重試</h5>
+                            <h5>您目前連線頻率過高，請稍後重試</h5>
                             @if(isset($user))
                                 {{ logger('429, user id: ' . $user->id . ', IP: ' . request()->ip()) }}
                             @else
