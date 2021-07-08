9c2162d646bb75125d56691f056daa9d575c0e85
yue <yueitng1214@gmail.com>
2021-07-08 11:05:40 +0800

    VIP女會員若沒勾選開放清晰照給VIP，那麼預覽自己會員頁時，也要是模糊的

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1035,17 +1035,14 @@ class UserService
     }
 
     public static function isBlurAvatar($to, $user) {
-      if($to->id == $user->id) return false;
+      //if($to->id == $user->id) return false;
       $isBlurAvatar = true;
       $blurryAvatar = isset($to->meta->blurryAvatar)? $to->meta->blurryAvatar : "";
       $blurryAvatar = explode(',', $blurryAvatar);
       if($user->meta->isWarned == 1 || $user->aw_relation){
           $isBlurAvatar = true;
       }else{
-          if ($user->engroup == 2){
-              $isBlurAvatar = false;
-          }
-          else if(sizeof($blurryAvatar)>1){
+          if(sizeof($blurryAvatar)>1){
               $nowB = $user->isVip()? 'VIP' : 'general';
               $isBlurAvatar = in_array($nowB, $blurryAvatar);
           }
@@ -1057,16 +1054,14 @@ class UserService
     }
 
     public static function isBlurLifePhoto($to, $user) {
-      if($to->id == $user->id) return false;
+      //if($to->id == $user->id) return false;
       $isBlurLifePhoto = true;
       $blurryLifePhoto = isset($to->meta->blurryLifePhoto)? $to->meta->blurryLifePhoto : "";
       $blurryLifePhoto = explode(',', $blurryLifePhoto);
       if($user->meta->isWarned == 1 || $user->aw_relation){
           $isBlurLifePhoto = true;
       }else{
-        if ($user->engroup == 2){
-            $isBlurLifePhoto = false;
-        }else if(sizeof($blurryLifePhoto)>1){
+        if(sizeof($blurryLifePhoto)>1){
             $nowB = $user->isVip()? 'VIP' : 'general';
             $isBlurLifePhoto = in_array($nowB, $blurryLifePhoto);
         }
