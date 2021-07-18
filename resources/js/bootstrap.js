20dc6ac3a02255cf2183d27b94550e6d3d420676
LZong <lzong.tw@gmail.com>
2021-07-18 16:21:45 +0800

    JS codes.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,6 +37,8 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true,
+    wsPort: 6001,
+    wsHost: window.location.hostname,
+    forceTLS: false,
     // authEndpoint: '/custom/endpoint/auth',
 });
