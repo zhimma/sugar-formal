366fb5b701e7afa9dd5736eeb1ae2bd02659d280
wayjen <iih1n9beb-els@yahoo.com>
2021-07-26 17:37:31 +0800

    fix endif location

--- resources/views/new/dashboard/chatWithUser.blade.php
+++ resources/views/new/dashboard/chatWithUser.blade.php
@@ -184,19 +184,19 @@
                 @if(isset($to))
 {{--                    <div class="fbuttop"></div>--}}
                     <div class="shouxq" style="display: flex;">
-						@if($to->id==$admin->id)
-                        <a class="nnn_adbut" href="{!! url('dashboard/personalPage') !!}">
-                            <img class="nnn_adbut_img" src="{{ asset('/new/images/back_icon.png') }}" style="height: 15px;">返回
-                        </a>
-                        <span style="flex: 6; text-align: center;">
-                            @if($to->id!=$admin->id)
-                            <a href="/dashboard/viewuser/{{$to->id}}" style="color: #fd5678;">{{$to->name}}</a>
+                        @if(isset($admin))
+                            @if($to->id == $admin->id)
+                                <a class="nnn_adbut" href="{!! url('dashboard/personalPage') !!}">
+                                    <img class="nnn_adbut_img" src="{{ asset('/new/images/back_icon.png') }}" style="height: 15px;">返回
+                                </a>
+                                <span style="flex: 6; text-align: center;">
+                                    @if($to->id != $admin->id)
+                                        <a href="/dashboard/viewuser/{{$to->id}}" style="color: #fd5678;">{{$to->name}}</a>
+                                    @else
+                                        <span  style="color: #fd5678;">系統來訊通知</span>
+                                    @endif
+                                </span>
                             @else
-                            <span  style="color: #fd5678;">系統來訊通知</span>
-                            @endif
-                        </span>
-                        @else
-
                         <a class="nnn_adbut" href="{{ !empty(session()->get('goBackPage_chat2')) ? session()->get('goBackPage_chat2') : \Illuminate\Support\Facades\URL::previous() }}"><img class="nnn_adbut_img" src="{{ asset('/new/images/back_icon.png') }}" style="height: 15px;">返回</a>
                         <span style="flex: 6; text-align: center;">
                             <a href="/dashboard/viewuser/{{$to->id}}" style="color: #fd5678;">
@@ -221,6 +221,7 @@
                             </form>
                         @else
                             <button style="float: right; position: relative;" type="button" class="paypay" onclick="c5('這是Daddy主動發起的，請提醒Daddy按此按紐發動車馬費邀請！')"><a class="nnn_adbut" style="margin-top: -15px">車馬費</a></button>
+                        @endif
                         @endif
                         @endif
                     </div>
