f93a217ad644eb117ea880169cee7d9e65b5aa8a
wayjen <iih1n9beb-els@yahoo.com>
2021-07-26 09:16:22 +0800

    fetch latest

--- app/Services/EsafePay.php
+++ app/Services/EsafePay.php
@@ -122,7 +122,7 @@ abstract class EsafePay_Aio
         return $rs;
     }
 
-    protected static function HtmlEncode($target = "_self", $arParameters, $ServiceURL, $paymentButton = '') {
+    protected static function HtmlEncode($target = "_self", $arParameters = "", $ServiceURL = "", $paymentButton = '') {
         //生成表單，自動送出
         $szHtml =  '<!DOCTYPE html>';
         $szHtml .= '<html>';
