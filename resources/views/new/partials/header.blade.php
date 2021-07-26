60cc66d09084c5102a86c3865f103ce16f5d48e5
wayjen <iih1n9beb-els@yahoo.com>
2021-07-26 11:22:08 +0800

    fetch

--- resources/views/new/partials/header.blade.php
+++ resources/views/new/partials/header.blade.php
@@ -6,7 +6,11 @@
 		<title>甜心花園包養網測試站</title>
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.28/webfontloader.js"></script>
-		<script src="/new/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+		@if (\App::environment('local'))
+			<script src="{{ asset('js/app_local.js') }}" type="text/javascript"></script>
+		@else
+			<script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+		@endif
 		<script src="/new/js/jquery.lazyload.min.js" type="text/javascript"></script>
 		<?php //新樣板css?>
 		<link href="/new/css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +23,6 @@
 		<link rel="stylesheet" href="/plugins/parsleyjs/parsley.css">
 		<link rel="stylesheet" href="/new/css/responsive_chat.css">
 		<?php //新樣板js?>
-		<script src="/new/js/bootstrap.min.js"></script>
 		<script src="/new/js/main.js" type="text/javascript"></script>
 		<script src="/new/js/loading.js" type="text/javascript"></script>
 		<script src="/plugins/sweetalert/sweetalert2.js" type="text/javascript"></script>
@@ -100,6 +103,118 @@
                         });
                     }
                 }
+				let users = null;
+				let users_leaving = null;
+				let BreakException = [];
+				{{-- Echo.join('Online'); --}}
+				@if(($isVip ?? false) || $user->isVip())
+					@if(str_contains(url()->current(), 'search') || request()->route()->getName() == 'chat2View')
+						{{-- $(document).ready(() => {
+							Echo.join('Online').here((users) => {
+								try {
+									let showedUsers = $(".searchStatus");
+									let chatUsers = $(".sjpic.shanx");
+									let userList;
+									if(showedUsers.length > 0){
+										userList = showedUsers
+									}
+									else{
+										userList = chatUsers;
+									}
+									users.forEach(function (user) {
+										$(userList).each((i, userListed) =>{
+											if (user['id'] == userListed.id) {
+												setUserOnlineStatus(1, user['id']);
+												throw BreakException;
+											}
+										})
+									});
+								} catch (e) {
+									if (e !== BreakException) throw e;
+								}
+							}).joining((user) => {
+								setUserOnlineStatus(1, user.id);
+							}).leaving((user) => {
+								setUserOnlineStatus(0, user.id);
+							});
+						}); --}}
+					@elseif(str_contains(url()->current(), 'viewuser') || request()->route()->getName() == 'chat2WithUser')
+						{{-- Echo.join('Online')
+							.here((users) => {
+								try {
+									users.forEach(function (user) {
+										@if(isset($to))
+										if (user['id'] == '{{ $to->id }}') {
+											setUserOnlineStatus(1);
+											throw BreakException;
+										}
+										@endif
+									});
+								} catch (e) {
+									if (e !== BreakException) throw e;
+								}
+							})
+							.joining((user) => {
+								@if(isset($to))
+								if (user.id == '{{ $to->id }}') {
+									setUserOnlineStatus(1);
+									return 0;
+								}
+								@endif
+							})
+							.leaving((user) => {
+								@if(isset($to))
+								if (user.id == '{{ $to->id }}') {
+									setUserOnlineStatus(0);
+									return 0;
+								}
+								@endif
+							}); --}}
+					@endif
+				@else
+						{{-- $(document).ready(() => {setUserOnlineStatus('Non-VIP') }); --}}
+				@endif
+				function setUserOnlineStatus(status, element_id){
+					{{-- onlineStatus: chatWithUser, onlineStatus2: viewuser, onlineStatusChatView: chatview --}}
+					if(status === 'Non-VIP'){
+						if($('#onlineStatus').length > 0){
+							$('#onlineStatus').addClass("onlineStatusNonVip");
+							$('#onlineStatus').prepend($('<img src="/new/images/wsx.png">'));
+						}
+						if($('#onlineStatusNonVip2').length > 0){
+							$('#onlineStatusNonVip2').show();
+						}
+						if(element_id){
+							{{-- $("#" + element_id).find('.onlineStatusChatView').addClass('nonVip');
+							$("#" + element_id).find('.onlineStatusChatView').prepend($('<img src="/new/images/wsx.png">')); --}}
+						}
+					}
+					else if(status){
+						if($('#onlineStatus').length > 0){
+							$('#onlineStatus').addClass("onlineStatus");
+						}
+						if($('#onlineStatus2').length > 0){
+							$('#onlineStatus2').show();
+						}
+						if(element_id){
+							$("#" + element_id).find('.onlineStatusChatView').addClass('online');
+							$(".searchStatus#" + element_id).addClass('onlineStatusSearch');
+						}
+					}
+					else{
+						if($('#onlineStatus').length > 0) {
+							$('#onlineStatus').removeClass('onlineStatus');
+						}
+						if($('#onlineStatus2').length > 0){
+							$('#onlineStatus2').hide();
+						}
+						if(element_id){
+							$("#" + element_id).find('.onlineStatusChatView').removeClass('online');
+							$(".searchStatus#" + element_id).removeClass('onlineStatusSearch');
+						}
+					}
+				}
 			</script>
+			@include('new.partials.unread')
 		@endif
 </head>
