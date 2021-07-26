fba40d3e1c602b3ad365f71b752954db65fe0aff
wayjen <iih1n9beb-els@yahoo.com>
2021-07-26 09:06:59 +0800

    fetch  latest

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use App\Observers\BannedObserver;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Factory;
-use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
