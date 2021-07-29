d178ea49b50892ffaeccd46bde534b3e3240f36f
LZong <lzong.tw@gmail.com>
2021-07-29 11:13:00 +0800

    ...

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -773,7 +773,7 @@ class Message extends Model
                         ->where([['m.is_row_delete_1','<>',$uid],['m.is_single_delete_1', '<>' ,$uid], ['m.all_delete_count', '<>' ,$uid],['m.is_row_delete_2', '<>' ,$uid],['m.is_single_delete_2', '<>' ,$uid],['m.temp_id', '=', 0]])
                         ->where('m.read', 'N')
                         ->where([['m.created_at','>=',self::$date]])
-						->where('u.engroup', '<>',$user->engroup)
+						->where('u.engroup', '<>', $user->engroup)
                         ->whereRaw('m.created_at < IFNULL(b1.created_at,"2999-12-31 23:59:59")')
                         ->whereRaw('m.created_at < IFNULL(b2.created_at,"2999-12-31 23:59:59")')
                         ->whereRaw('m.created_at < IFNULL(b3.created_at,"2999-12-31 23:59:59")')
@@ -865,7 +865,7 @@ class Message extends Model
         $query->whereRaw('m.created_at < IFNULL(b2.created_at,"2999-12-31 23:59:59")');
         $query->whereRaw('m.created_at < IFNULL(b3.created_at,"2999-12-31 23:59:59")');
         $query->whereRaw('m.created_at < IFNULL(b4.created_at,"2999-12-31 23:59:59")');
-        $query->whereRaw('u1.engroup!=u2.engroup');
+        $query->whereRaw('u1.engroup != ' . $user->engroup);
 
         $allMessageCount = $all_msg->count();
 
