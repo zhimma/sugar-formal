e70b9ca275ed419fa247da4409818e6b3d9fa148
yue <yueitng1214@gmail.com>
2021-09-10 08:41:08 +0800

    後台頁面調整 1.Ip列表頁面 2.進階會員搜尋調整 3.菜單項目調整

--- resources/views/admin/users/advIndex.blade.php
+++ resources/views/admin/users/advIndex.blade.php
@@ -2,7 +2,7 @@
 @section('app-content')
 <body style="padding: 15px;">
 <h1>進階會員搜尋</h1>
-<form method="POST" action="{{ route('users/advSearch') }}" class="search_form">
+<form method="get" action="{{ route('users/advSearch') }}" class="search_form">
 	{!! csrf_field() !!}
 	<div class="form-group">
 		<table class="table table-bordered table-hover">
@@ -24,12 +24,42 @@
             </tr>
             <tr>
                 <th>
-                    <label for="keyword" class="">關鍵字</label>
+                    <label for="keyword" class="">關鍵字</label><!--(關於我、約會模式)-->
                 </th>
                 <td>
-                    <input type="text" name='keyword' class="" style="width:300px;" id="keyword" value="@if(isset($keyword)){{ $keyword }}@endif">
+                    <input type="text" name='keyword' class="" style="width:300px;" id="keyword" value="@if(isset($keyword)){{ $keyword }}@endif" autocomplete="off">
+                    <div id="assgin_login_time" @if(!isset($keyword)) style="display: none;" @endif >
+                        <input type="radio" name="login_time" value="3days" @if($login_time=='3days') checked="true" @endif/>三天
+                        <input type="radio" name="login_time" value="1week" @if($login_time=='1week') checked="true" @endif/>一週
+                        <input type="radio" name="login_time" value="2weeks" @if($login_time=='2weeks') checked="true" @endif/>兩週
+                    </div>
                 </td>
             </tr>
+            <tr>
+                <th>
+                    <label for="phone" class="">註冊手機</label>
+                </th>
+                <td>
+                    <input type="text" name='phone' class="" style="width:300px;" id="phone" value="@if(isset($phone)){{ $phone }}@endif">
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="title" class="">一句話</label>
+                </th>
+                <td>
+                    <input type="text" name='title' class="" style="width:300px;" id="title" value="@if(isset($title)){{ $title }}@endif">
+                </td>
+            </tr>
+            <tr>
+                <th>
+                    <label for="order_no" class="">帳單查詢</label>
+                </th>
+                <td>
+                    <input type="text" name='order_no' class="" style="width:300px;" id="title" value="@if(isset($order_no)){{ $order_no }}@endif">
+                </td>
+            </tr>
+
             <tr>
                 <th>排序方式1</th>
                 <td>
@@ -54,60 +84,149 @@
 </form><br>
 @if(isset($users))
 <table class='table table-hover table-bordered' id="tableList">
-	<tr>
-		<td>會員ID</td>
-		<td>暱稱</td>
-		<td>標題</td>
-        <td>關於我</td>
-        <td>希望的約會模式</td>
-		<td>男/女</td>
-		<td>Email</td>
-		<td>建立時間</td>
-		<td>更新時間</td>
-		<td>上次登入</td>
-		<td>封鎖使用者</td>
-        <td>站長訊息</td>
-		<td>所有資料/管理</td>
-	</tr>
-	@forelse ($users as $user)
-	<tr @if($user['isBlocked']==1) style="color: #FF0000;" @endif>
-		<td class="align-middle">{{ $user['id'] }}</td>
-		<td class="align-middle">{{ $user['name'] }} @if($user['vip'] == 1) <i class="m-nav__link-icon fa fa-diamond"></i>@endif @if(isset($user['vip_data']) && $user['vip_data']['expiry'] != "0000-00-00 00:00:00") (到期日: {{ substr($user['vip_data']['expiry'], 0, 10) }}) @endif</td>
-		<td class="align-middle">{{ $user['title'] }}</td>
-        <td class="align-middle">{{ $user['about'] }}</td>
-        <td class="align-middle">{{ $user['style'] }}</td>
-		<td class="align-middle">@if($user['engroup']==1) 男 @else 女 @endif</td>
-		<td class="align-middle">{{ $user['email'] }}</td>
-		<td class="align-middle">{{ $user['created_at'] }}</td>
-		<td class="align-middle">{{ $user['updated_at'] }}</td>
-		<td class="align-middle">{{ $user['last_login'] }}</td>
-        <td class="align-middle">
-            <form action="toggleUserBlock" method="POST">{!! csrf_field() !!}
-                <input type="hidden" value="@if(isset($email )){{ $email }}@endif" name="email">
-                <input type="hidden" value="@if(isset($name)){{ $name }}@endif" name="name">
-                <input type="hidden" value="{{ $user['id'] }}" name="user_id">
-                @if($user['isBlocked'] == 1)
-                    <button type="submit" class='text-white btn @if($user['isBlocked'] == 1) btn-success @else btn-danger @endif'> 解除 </button>
-                @else
-                    <a class="btn btn-danger ban-user" href="#" data-toggle="modal" data-target="#blockade" data-id="{{ $user['id'] }}" data-sname="@if(isset($name)){{ $name }}@endif" data-email="@if(isset($email )){{ $email }}@endif"  data-name="{{ $user['name'] }}" onclick="setBlockade(this)">封鎖</a>
-                @endif
-            </form>
-        </td>
-        <td class="align-middle">
-            <a href="message/to/{{ $user['id'] }}" target="_blank" class='btn btn-dark'>撰寫</a>
-        </td>
-        <td class="align-middle"><a href="advInfo/{{ $user['id'] }}" target='_blank' class='text-white btn btn-primary'>前往</a></td>
-	</tr>
-	@empty
-	<tr>找不到符合條件的資料</tr>
-	@endforelse
+    <tr>
+        <th>ID</th>
+        <th>Email</th>
+        <th>名稱</th>
+        <th>男/女</th>
+        <th>是否為VIP</th>
+        <th>是否為免費方案</th>
+        <th>升級時的帳單編號</th>
+        <th>付費方式</th>
+        <th>VIP資料建立時間</th>
+        <th>VIP資料更新時間</th>
+        <th>變更男/女</th>
+        <th>提供/取消VIP權限</th>
+    </tr>
+    @forelse ($users as $user)
+        @php
+            $result['isBlocked'] = \App\Models\SimpleTables\banned_users::where('member_id', 'like', $user->id)->get()->first();
+            if(!isset($result['isBlocked'])){
+                $result['isBlocked'] = \App\Models\BannedUsersImplicitly::select(\DB::raw('id, "隱性" as type'))->where('target', 'like', $user->id)->get()->first();
+            }
+            $userInfo=\App\Models\User::findById($user->id);
+            $user['name'] = $userInfo->name;
+            $user['engroup'] = $userInfo->engroup;
+            $user['last_login'] = $userInfo->last_login;
+            $user['vip'] = \App\Models\Vip::vip_diamond($userInfo->id);
+            $user['tipcount'] = \App\Models\Tip::TipCount_ChangeGood($userInfo->id);
+            $user['exchange_period'] = $userInfo->exchange_period;
+            $user['warnedicon'] = \App\Models\User::warned_icondata($user->id);
+
+        @endphp
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->email }}</td>
+            <td @if($result['isBlocked']) style="background-color:#FFFF00" @endif>
+                <a href="{{ route('users/advInfo', $user->id) }}" target='_blank'>
+                    <p @if($user['engroup'] == '2') style="color: #F00;" @else  style="color: #5867DD;"  @endif>
+                        {{ $user->name }}
+                        @if($user['vip'])
+                            @if($user['vip']=='diamond_black')
+                                <img src="/img/diamond_black.png" style="height: 16px;width: 16px;">
+                            @else
+                                @for($z = 0; $z < $user['vip']; $z++)
+                                    <img src="/img/diamond.png" style="height: 16px;width: 16px;">
+                                @endfor
+                            @endif
+                        @endif
+                        @if(isset($user['tipcount']))
+                            @for($i = 0; $i < $user['tipcount']; $i++)
+                                👍
+                            @endfor
+                        @else
+                            {{ logger('reportedUsers, line 80 tipcount does not exists, user id: ' . $result['reported_id']) }}
+                        @endif
+                        @if(!is_null($result['isBlocked']))
+                            @if(!is_null($result['isBlocked']['expire_date']))
+                                @if(round((strtotime($result['isBlocked']['expire_date']) - getdate()[0])/3600/24)>0)
+                                    {{ round((strtotime($result['isBlocked']['expire_date']) - getdate()[0])/3600/24 ) }}天
+                                @else
+                                    此會員登入後將自動解除封鎖
+                                @endif
+                            @elseif(isset($result['isBlocked']['type']))
+                                (隱性)
+                            @else
+                                (永久)
+                            @endif
+                        @endif
+                        @if($user['warnedicon']['isAdminWarned']==1 OR $user['warnedicon']['isWarned']==1)
+                            <img src="/img/warned_red.png" style="height: 16px;width: 16px;">
+                        @endif
+                        @if($user['warnedicon']['isWarned']==0 AND $user['warnedicon']['WarnedScore']>10 AND $user['warnedicon']['auth_status']==1)
+                            <img src="/img/warned_black.png" style="height: 16px;width: 16px;">
+                        @endif
+                    </p>
+                </a>
+            </td>
+            <td>{{ $user->engroup ==1 ? '男':'女' }}</td>
+            @if($userInfo->isVip())
+                <td>是 @if($user->vip_data->expiry!="0000-00-00 00:00:00") (到期日: {{ substr($user->vip_data->expiry, 0, 10) }}) @endif</td>
+                <td>@if($user->vip_data->free == 1) 是 @else 否 @endif</td>
+                <td>{{ $user->vip_order_id }}</td>
+                <td>
+                    @if ($user->vip_data->payment_method == 'CREDIT')
+                        信用卡
+                    @elseif ($user->vip_data->payment_method == 'ATM')
+                        ATM
+                    @elseif ($user->vip_data->payment_method == 'CVS')
+                        超商代碼
+                    @elseif ($user->vip_data->payment_method == 'BARCODE')
+                        超商條碼
+                    @endif
+                </td>
+            @else
+                <td>否</td>
+                <td>@if(isset($user->vip_data))@if($user->vip_data->free == 1) 是 @else 否 @endif @else 無資料 @endif</td>
+                <td>@if(isset($user->vip_order_id)){{ $user->vip_order_id }}@else 無資料 @endif</td>
+                <td>@if(isset( $user->vip_data->payment_method))
+                        @if ($user->vip_data->payment_method == 'CREDIT')
+                            信用卡
+                        @elseif ($user->vip_data->payment_method == 'ATM')
+                            ATM
+                        @elseif ($user->vip_data->payment_method == 'CVS')
+                            超商代碼
+                        @elseif ($user->vip_data->payment_method == 'BARCODE')
+                            超商條碼
+                        @endif
+                    @else 無資料
+                    @endif
+                </td>
+            @endif
+            <td>@if(isset($user->vip_data->created_at))
+                    <a href="{{ route('stats/vip_log', $user->id) }}" target="_blank">
+                        				<a href="{{ url('admin/order#'.$user->email) }}" target="_blank">
+                        {{ $user->vip_data->created_at }}</a>@else 無資料 @endif</td>
+            <td>@if(isset($user['updated_at'])){{ $user['updated_at'] }}@else 無資料 @endif</td>
+            <td>
+                <form method="POST" action="genderToggler" class="user_profile">{!! csrf_field() !!}
+                    <input type="hidden" name='page' value="back">
+                    <input type="hidden" name='user_id' value="{{ $user->id }}">
+                    <input type="hidden" name='gender_now' value="{{ $user->engroup }}">
+                    <button type="submit" class="btn btn-warning">變更</button></form>
+            </td>
+            <td>
+                <form method="POST" action="VIPToggler" class="vip">{!! csrf_field() !!}
+                    <input type="hidden" name='page' value="back">
+                    <input type="hidden" name='user_id' value="{{ $user->id }}">
+                    <input type="hidden" name='isVip' value="@if($userInfo->isVip()) 1 @else 0 @endif">
+                    <button type="submit" class="btn btn-info">@if($userInfo->isVip()) 取消權限 @else 提供權限 @endif</button></form>
+            </td>
+        </tr>
+    @empty
+        <tr>
+            找不到資料
+        </tr>
+    @endforelse
 </table>
-<div align="center">
-    <input type="hidden" value="2" id="morePage">
-    @if(!isset($email) && !isset($name) && !isset($keyword))
-    <button class="btn btn-info" onclick="getUserInfo()">載入更多</button>
-    @endif
-</div>
+    {!! $users->appends(request()->input())->links('pagination::sg-pages') !!}
+
+{{--<div align="center">--}}
+{{--    <input type="hidden" value="2" id="morePage">--}}
+{{--    @if(!isset($email) && !isset($name) && !isset($keyword))--}}
+{{--    <button class="btn btn-info" onclick="getUserInfo()">載入更多</button>--}}
+{{--    @endif--}}
+{{--</div>--}}
 @endif
 </body>
 </html>
@@ -235,5 +354,15 @@
             }
         })
     }
+
+    $("input[name='keyword']").keyup(function(){
+
+        if($(this).val()){
+            $('#assgin_login_time').show();
+        }else{
+            $('#assgin_login_time').hide();
+        }
+
+    });
 </script>
 @stop
