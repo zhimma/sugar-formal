2faef9d416747459b28f28e7c1dd8938959be872
Linna <ellelinna@gmail.com>
2021-10-07 18:17:19 +0800

    Let OrderGenerator in Schedule.

--- app/Providers/ScheduleServiceProvider.php
+++ app/Providers/ScheduleServiceProvider.php
@@ -14,6 +14,7 @@ class ScheduleServiceProvider extends ServiceProvider
      */
     protected $commands = [
         \App\Console\Commands\InsertPR::class,
+        \App\Console\Commands\OrderGenerator::class
     ];
     public function boot()
     {
@@ -21,6 +22,7 @@ class ScheduleServiceProvider extends ServiceProvider
         $this->app->booted(function () {
             $schedule = $this->app->make(Schedule::class);
             $schedule->command(\App\Console\Commands\InsertPR::class)->timezone('Asia/Taipei')->dailyAt('04:00');
+            $schedule->command(\App\Console\Commands\OrderGenerator::class)->timezone('Asia/Taipei')->dailyAt('06:00');
         });
     }
 
