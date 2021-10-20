ed93832b05ad9d0eebc42454ebee2b659970c9e7
LZong <lzong.tw@gmail.com>
2021-10-21 01:12:52 +0800

    Bug fixed.

--- app/Facades/DBConfig.php
+++ app/Facades/DBConfig.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade;
 
 class DBConfig extends Facade
 {
-    public static function db_config($key) {
-        \DB::table("queue_global_variables")->where("name", $key)->first()->value ?? config('social.send-email') ?? false;
+    public static function get($key) {
+        return \DB::table("queue_global_variables")->where("name", $key)->first()->value ?? config('social.send-email') ?? false;
     }
 }
