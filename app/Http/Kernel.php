b58c4b26bf512e5278cc160de5d80fa5256aeb29
LZong <lzong.tw@gmail.com>
2021-12-02 20:28:35 +0800

    Monitoring system added.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\IPAddressesAllow::class,
+        \App\Http\Middleware\IPAddressesAllow::class,        \Inspector\Laravel\Middleware\WebRequestMonitoring::class,
     ];
 
     /**
