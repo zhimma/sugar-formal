4ea337d1522f4dc8c8454a280d8a28a7025a8751
LZong <lzong.tw@gmail.com>
2021-12-06 12:39:23 +0800

    Short open tag issues modified, small bug fixed.

--- resources/views/new/auth/login.blade.php
+++ resources/views/new/auth/login.blade.php
@@ -1,4 +1,4 @@
-<?
+<?php
 header("Cache-Control: no-cache, no-store, must-revalidate, post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
