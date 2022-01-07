0df859065ebcf498b16ec6ea502296e2a90c687f
minfan <shoyou123@gmail.com>
2022-01-07 08:35:20 +0000

    Babel modified.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,8 +13,20 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .polyfill({
-       enabled: true,
-       useBuiltIns: "usage"
-    })
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+.webpackConfig({
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+});
