a0511e49ca45bd4152d810b4f6834ceda6380052
simon821010 <simon821010@gmail.com>
2022-01-21 18:07:37 +0800

    2022-01-21-1807-拒絕區域及海外

--- resources/views/new/dashboard.blade.php
+++ resources/views/new/dashboard.blade.php
@@ -4,6 +4,7 @@ header("Pragma: no-cache");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
 ?>
+
 @extends('new.layouts.website')
 @section('app-content')
 
