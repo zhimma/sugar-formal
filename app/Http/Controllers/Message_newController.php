0fdcd1224fb1a655a294678ff71e4dc254b26b28
LZong <lzong.tw@gmail.com>
2022-02-07 13:46:42 +0800

    Rate limitation modified.

--- app/Http/Controllers/Message_newController.php
+++ app/Http/Controllers/Message_newController.php
@@ -34,8 +34,8 @@ class Message_newController extends BaseController {
     public function __construct(UserService $userService) {
         parent::__construct();
         $this->service = $userService;
-        $this->middleware('throttle:140,1');
-        $this->middleware('pseudoThrottle:100,1');
+        $this->middleware('throttle:180,1');
+        $this->middleware('pseudoThrottle:140,1');
     }
 
     // handle delete message
