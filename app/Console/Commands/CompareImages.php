5e89360eed986e90df7deec831b80dcb67ad3404
LZong <lzong.tw@gmail.com>
2022-02-07 19:30:04 +0800

    Division by zero avoided.

--- app/Console/Commands/CompareImages.php
+++ app/Console/Commands/CompareImages.php
@@ -242,15 +242,20 @@ class CompareImages extends Command
                         $targetInterset = array_intersect_key($targetEncode,$srcEncode);                        
 
                         $srcPartInterDiffSum = 0;
-                        $i=0;
-                        $srcIntersetSum=0;
-                        foreach($srcInterset  as $ki=>$vi) {  
+                        $i = 0;
+                        $srcIntersetSum = 0;
+                        foreach($srcInterset  as $ki => $vi) {  
                             if($i>=10) break;
                             $srcPartInterDiffSum+= ($vi-$targetEncode[$ki]>0)?($vi-$targetEncode[$ki]):0;
                             $srcIntersetSum+=$vi;
                             $i++;
                         }
-                        $srcInterPercent =100-( 100*$srcPartInterDiffSum/ $srcIntersetSum);
+
+                        if($srcIntersetSum == 0) {
+                            // 避免 ÷ 0 的問題發生
+                            continue;
+                        }
+                        $srcInterPercent = 100 - ( 100 * $srcPartInterDiffSum / $srcIntersetSum);
 
                         $targetPartInterDiffSum = 0;
                         $i=0;
