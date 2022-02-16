c81442c2015898d1ad0b7acff7acf077d4d622a8
simon821010 <simon821010@gmail.com>
2022-02-16 16:17:04 +0800

    2022-02-16-1616-feat-新增db與model

---
+++ app/Models/inbox_refuse_set.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class inbox_refuse_set extends Model
+{
+    protected $table = 'inbox_refuse_set';
+}
---
+++ database/migrations/2022_02_16_155031_create_inbox_refuse_set_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInboxRefuseSetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inbox_refuse_set', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->boolean('refuse_vip')->default(0);
+            $table->boolean('refuse_common')->default(0);
+            $table->boolean('refuse_warned')->default(0);
+            $table->integer('refuse_PR')->default(0);
+            $table->integer('refuse_canned_message_PR')->default(0);
+            $table->integer('refuse_register_days')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inbox_refuse_set');
+    }
+}
