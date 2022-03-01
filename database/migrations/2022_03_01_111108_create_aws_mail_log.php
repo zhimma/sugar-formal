88ff05bccf16ced8c698da4fac3fd587c1c9e54b
simon821010 <simon821010@gmail.com>
2022-03-01 14:04:57 +0800

    2022-03-01-1404-暫存

---
+++ database/migrations/2022_03_01_111108_create_aws_mail_log.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAwsMailLog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aws_ses_mail_log', function (Blueprint $table) {
+            $table->id();
+            $table->string('notificationtype');
+            $table->json('mail');
+            $table->json('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aws_ses_mail_log');
+    }
+}
