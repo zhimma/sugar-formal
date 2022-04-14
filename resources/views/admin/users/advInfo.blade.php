5d44ca6469a0f269a279eed6ce5e37b8e2e179b9
simon821010 <simon821010@gmail.com>
2022-04-14 11:08:19 +0800

    2022-04-14-1108-隱藏按鈕

--- resources/views/admin/users/advInfo.blade.php
+++ resources/views/admin/users/advInfo.blade.php
@@ -1607,11 +1607,9 @@
 </style>
 <h4>
 	所有訊息
-	@if($userMessage_log->isEmpty()) 
-		<button id='message_show_btn' class='btn btn-primary' style="width:80px;">顯示</button>
-	@endif
+	<button id='message_show_btn' class='btn btn-primary' style="width:80px;">顯示</button>
 </h4>
-<table id="m_log" class="table table-hover table-bordered" @if($userMessage_log->isEmpty()) style="display:none" @endif>
+<table id="m_log" class="table table-hover table-bordered">
 	<tr>
 		<th width="5%"></th>
 		<th width="10%">發送給</th>
@@ -1621,22 +1619,22 @@
 		<th width="8%">發送數 <br>本人/對方</th>
 	</tr>
 	@foreach($userMessage_log as $Log)
-		<tr>
-			@php
-				$ref_user=\App\Models\User::findById($Log->ref_user_id);
-				$ref_user_id=$Log->ref_user_id;
-				$message_log=\App\Models\Message::withTrashed()
-					->where([['message.to_id', ($ref_user->engroup==1 ? $ref_user->id : $user->id)],['message.from_id', ($ref_user->engroup==1 ? $user->id : $ref_user->id)]])
-					->orderBy('created_at')->first();
+		@php
+			$ref_user=\App\Models\User::findById($Log->ref_user_id);
+			$ref_user_id=$Log->ref_user_id;
+			$message_log=\App\Models\Message::withTrashed()
+				->where([['message.to_id', ($ref_user->engroup==1 ? $ref_user->id : $user->id)],['message.from_id', ($ref_user->engroup==1 ? $user->id : $ref_user->id)]])
+				->orderBy('created_at')->first();
 
-				$message_1st=\App\Models\Message::withTrashed()
-					->where([['message.to_id', ($ref_user->engroup==1 ? $ref_user->id : $user->id)],['message.from_id', ($ref_user->engroup==1 ? $user->id : $ref_user->id)]])
-					->orWhere([['message.from_id', ($ref_user->engroup==1 ? $ref_user->id : $user->id)],['message.to_id', ($ref_user->engroup==1 ? $user->id : $ref_user->id)]])
-					->orderBy('created_at')->first();
+			$message_1st=\App\Models\Message::withTrashed()
+				->where([['message.to_id', ($ref_user->engroup==1 ? $ref_user->id : $user->id)],['message.from_id', ($ref_user->engroup==1 ? $user->id : $ref_user->id)]])
+				->orWhere([['message.from_id', ($ref_user->engroup==1 ? $ref_user->id : $user->id)],['message.to_id', ($ref_user->engroup==1 ? $user->id : $ref_user->id)]])
+				->orderBy('created_at')->first();
 
-				$toCount_user_id=\App\Models\Message::withTrashed()->where('from_id',$user->id)->where('to_id',$ref_user_id)->get()->count();
-				$toCount_ref_user_id=\App\Models\Message::withTrashed()->where('from_id',$ref_user_id)->where('to_id',$user->id)->get()->count();
-			@endphp
+			$toCount_user_id=\App\Models\Message::withTrashed()->where('from_id',$user->id)->where('to_id',$ref_user_id)->get()->count();
+			$toCount_ref_user_id=\App\Models\Message::withTrashed()->where('from_id',$ref_user_id)->where('to_id',$user->id)->get()->count();
+		@endphp
+		<tr @if(!($message_log??false)) class='message_no_interactive' style="display:none" @endif>
 			<td style="text-align: center;"><button data-toggle="collapse" data-target="#msgLog{{$ref_user_id}}" class="accordion-toggle btn btn-primary message_toggle">+</button></td>
 			<td>@if(!empty($ref_user->name))<a href="{{ route('admin/showMessagesBetween', [$user->id, $ref_user_id]) }}" target="_blank">{{ $ref_user->name }}</a>@else 會員資料已刪除@endif</td>
 			<td id="new{{$Log->to_id}}">
@@ -1681,12 +1679,12 @@
 					</thead>
 					<tbody>
 					@foreach ($Log->items as $key => $item)
-{{--						@if($key==0)--}}
-{{--							<script>--}}
-{{--								$('#new' + {{$Log->to_id}}).text('{{ $item->content }}');--}}
-{{--								$('#new_time' + {{$Log->to_id}}).text('{{ $item->m_time }}');--}}
-{{--							</script>--}}
-{{--						@endif--}}
+						{{--@if($key==0)--}}
+							{{--<script>--}}
+								{{--$('#new' + {{$Log->to_id}}).text('{{ $item->content }}');--}}
+								{{--$('#new_time' + {{$Log->to_id}}).text('{{ $item->m_time }}');--}}
+							{{--</script>--}}
+						{{--@endif--}}
 						<tr>
 							<td style="text-align: right;">
 								@php
@@ -2609,7 +2607,16 @@ $("input[name='phone']").keyup(function(){
 	}
 
 	$('#message_show_btn').on('click', function(){
-		$('#m_log').show();
+		$('.message_no_interactive').toggle();
+		if($(this).text() == '顯示')
+		{
+			$(this).text('隱藏');
+		}
+		else if($(this).text() == '隱藏')
+		{
+			$(this).text('顯示');
+		}
+
 	});
 </script>
 <!--照片查看end-->
