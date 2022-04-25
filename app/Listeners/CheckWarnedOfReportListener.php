69661f7f2d6f84dddc3aa38d8fe5428cac78a06c
simon821010 <simon821010@gmail.com>
2022-04-25 20:38:57 +0800

    2022-04-25-2038-修正

--- app/Listeners/CheckWarnedOfReportListener.php
+++ app/Listeners/CheckWarnedOfReportListener.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use App\Models\UserMeta;
 use App\Models\User;
+use Carbon\Carbon;
 
 class CheckWarnedOfReportListener
 {
