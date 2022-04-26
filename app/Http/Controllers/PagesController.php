d47054f18eae46f2800d121ad657e44313a3fb26
LZong <lzong.tw@gmail.com>
2022-04-26 18:19:58 +0800

    Todo removed.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -686,7 +686,7 @@ class PagesController extends BaseController
 
     public function dashboard(Request $request)
     {
-        // todo: 驗證 VIP 是否成功付款
+        // 驗證 VIP 是否成功付款
         //      1. 綠界：連 API 檢查，使用 Laravel Queue 執行檢查
         //      2. 藍新：後台手動
         
