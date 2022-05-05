5586da522f097c18fb18f2f2f936fd7eb759594b
henry <hho345883@gmail.com>
2022-05-05 10:49:21 +0800

    Add data to message tables

---
+++ database/migrations/2022_05_04_210357_insert_data_to_message_room.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\MessageRoomUserXref;
+use App\Models\MessageRoom;
+use App\Models\Message;
+class InsertDataToMessageRoom extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $messagesList = DB::table('message')->orderBy('id','desc')->limit(10000)->chunk(1000, function($messages) {
+
+            foreach($messages->lazy() as $row){
+
+                $rows = array(
+                    $row->from_id,
+                    $row->to_id
+                );
+
+                $checkData = MessageRoomUserXref::whereIn('user_id',$rows)->groupBy('room_id')->havingRaw('count(user_id) = ?', [2]);
+        
+                if($checkData->count()==0){
+                    $messageRoom = new MessageRoom;
+                    $messageRoom->save();
+                    $room_id = $messageRoom->id;
+                
+        
+                    foreach($rows as $row){
+                        $messageRoomUserXref = new MessageRoomUserXref;
+                        $messageRoomUserXref->user_id = $row;
+                        $messageRoomUserXref->room_id = $room_id;
+                        $messageRoomUserXref->save();
+                    }
+
+                }else{
+                    $room_id = $checkData->first()['room_id'];
+                }    
+
+                //sometime it will happen error
+                DB::table('message')->where('id', $row->id)->update(['room_id'=>$room_id]);
+            }
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('message_room', function (Blueprint $table) {
+            //
+        });
+    }
+}
