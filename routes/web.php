7fa8b56ab6b329b2de5c25a16b5b7ba1f979d359
LZong <lzong.tw@gmail.com>
2022-05-13 13:39:08 +0800

    Visitor ID function removed, minor bug fixed.
    This reverts commit 5bbb6bc9a122df2773d5ec89b21d19a936639bfb.

--- routes/web.php
+++ routes/web.php
@@ -169,9 +169,6 @@ Route::group(['middleware' => ['auth', 'global','SessionExpired']], function () 
     Route::post('/multiple-login', 'PagesController@multipleLogin')->name('multipleLogin');
     Route::post('/save-cfp', 'PagesController@savecfp')->name('savecfp');
     Route::post('/check-cfp', 'PagesController@checkcfp')->name('checkcfp');
-
-    Route::post('/save-visitor-id', 'PagesController@saveVisitorID')->name('saveVisitorID');
-    Route::post('/check-visitor-id', 'PagesController@checkVisitorID')->name('checkVisitorID');
 });
 Route::post('/dashboard/faq_reply', 'PagesController@checkFaqAnswer')->middleware('auth')->name('checkFaqAnswer');
 Route::get('/advance_auth_activate/token/{token}', 'PagesController@advance_auth_email_activate')->name('advance_auth_email_activate');
