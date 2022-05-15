607be9043bc52036cfda9ec647cea9d8cd3d687e
simon821010 <simon821010@gmail.com>
2022-05-16 04:04:28 +0800

    2022-05-16-0404-暫存

--- resources/views/video-chat.blade.php
+++ resources/views/video-chat.blade.php
@@ -5,8 +5,20 @@
             <div class="col-sm-2 col-xs-2 col-md-2 dinone">
                 @include('new.dashboard.panel')
             </div>
-            <video-chat :allusers="{{ $users }}" :authUserId="{{ auth()->id() }}" turn_url="{{ env('TURN_SERVER_URL') }}"
-                turn_username="{{ env('TURN_SERVER_USERNAME') }}" turn_credential="{{ env('TURN_SERVER_CREDENTIAL') }}" />
+            
+            <div id="app">
+                <video-chat :allusers="{{ $users }}" :authUserId="{{ auth()->id() }}" turn_url="{{ env('TURN_SERVER_URL') }}"
+                    turn_username="{{ env('TURN_SERVER_USERNAME') }}" turn_credential="{{ env('TURN_SERVER_CREDENTIAL') }}" />
+            </div>
         <div>
     <div>
+    <script>
+        var data = {
+            message:'Hello World!'
+        }
+        new Vue({
+            el:'#app',
+            data:data
+        });
+    </script>
 @stop
