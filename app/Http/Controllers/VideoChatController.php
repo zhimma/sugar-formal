464353eb681b2676dd26ceaac62636cbf150288d
simon821010 <simon821010@gmail.com>
2022-05-18 11:57:58 +0800

    2022-05-18-1157-移除log

--- app/Http/Controllers/VideoChatController.php
+++ app/Http/Controllers/VideoChatController.php
@@ -14,22 +14,17 @@ class VideoChatController extends Controller
 {
     public function callUser(Request $request)
     {
-        Log::Info('callUser');
         $data['userToCall'] = $request->user_to_call;
         $data['signalData'] = $request->signal_data;
         $data['from'] = Auth::id();
         $data['type'] = 'incomingCall';
-        Log::Info($data);
-
         broadcast(new StartVideoChat($data))->toOthers();
     }
     public function acceptCall(Request $request)
     {
-        Log::Info('acceptCall');
         $data['signal'] = $request->signal;
         $data['to'] = $request->to;
         $data['type'] = 'callAccepted';
-        Log::Info($data);
         broadcast(new StartVideoChat($data))->toOthers();
     }
 
