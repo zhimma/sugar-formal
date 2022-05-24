cae243e79b14b92fa6e1fb949f5b5995afc1e230
simon821010 <simon821010@gmail.com>
2022-05-25 00:41:15 +0800

    2022-05-25-0041-修正

--- resources/js/components/VideoChat.vue
+++ resources/js/components/VideoChat.vue
@@ -88,7 +88,7 @@
             <button
               type="button"
               class="btn btn-success ml-5"
-              @click="acceptCall(callerDetails.id)"
+              @click="acceptCall({{ callerDetails.id }})"
             >
               接受
             </button>
