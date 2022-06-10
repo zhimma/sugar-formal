0d8e703f0836e5f30ac9e8766c4f86f0b91fe87d
LZong <lzong.tw@gmail.com>
2022-06-11 01:07:57 +0800

    Evaluation soft delete returns.

--- app/Models/Evaluation.php
+++ app/Models/Evaluation.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Evaluation extends Model
 {
-    // use SoftDeletes;
+    use SoftDeletes;
     protected $table = 'evaluation';
     protected $fillable = [
         'from_id',
