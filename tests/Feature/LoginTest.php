2554318c7daff3713d1279824b002d9204566457
henry <hho345883@gmail.com>
2022-06-30 02:33:51 +0800

    Add relation of login Test

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -18,4 +18,18 @@ class LoginTest extends TestCase
         $response = $this->get('/login');
         $response->assertStatus(200);
     }
+
+    public function test_users_can_authenticate_using_the_login_screen()
+    { 
+        $response = $this->postJson('/login', ['email' => 'TESTmaleVIP@test.com', 'password'=>'123123']);
+
+        $this->assertAuthenticated();
+    }
+
+    public function test_users_can_not_authenticate_with_invalid_password()
+    {
+        $response = $this->postJson('/login', ['email' => 'TESTmaleVIP@test.com', 'password'=>'123123']);
+
+        $this->assertGuest();
+    }
 }
