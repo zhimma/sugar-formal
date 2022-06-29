b9b4d10148a50c17eef4e468b43a7f092c6b2cfd
simon821010 <simon821010@gmail.com>
2022-06-29 11:26:27 +0800

    2022-06-29-1126-圖片檔名封鎖

--- app/Models/SetAutoBan.php
+++ app/Models/SetAutoBan.php
@@ -294,6 +294,17 @@ class SetAutoBan extends Model
                 case 'userAgent':
                     if(LogUserLogin::where('user_id',$uid)->where('userAgent', 'like','%'.$content.'%')->first() != null) $violation = true;
                     break;
+
+                //20220629新增圖片檔名
+                case 'picname':
+                    Log::info('start_pic_auto_ban');
+                    if(UserMeta::where('user_id',$uid)->where('pic_original_name','like','%'.$content.'%')->first() != null) $violation = true;
+
+                    //有一筆違規就可以封鎖了
+                    if(MemberPic::where('user_id',$uid)->where('original_name','like','%'.$content.'%')->first() != null) $violation = true;
+                    break;
+                //20220629新增圖片檔名   
+
                 case 'pic':
                     $ban_encode_entry = ImagesCompareService::getCompareEncodeByPic($content);
 
