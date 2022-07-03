2ad7cff36116986a64bbef0a6f9ef779eb3ac592
LZong <lzong.tw@gmail.com>
2022-07-04 01:54:38 +0800

    Evaluating stat.

--- routes/web.php
+++ routes/web.php
@@ -938,12 +938,33 @@ Route::group(['middleware' => ['auth', 'global', 'active', 'femaleActive', 'vipC
                 ->get()
                 ->pluck('user_id');
 
-            $receivers_by_user = Message::whereIn("from_id", $users_id)->keyBy("from_id")->pluck("to_id")->get();
+            $messages = Message::whereIn("from_id", $users_id)
+                ->where('created_at', '>=', '2022-06-25 14:00:00')
+                ->where('created_at', '<=', '2022-06-25 15:00:00')
+                ->get()->toArray();
+            
+            // 不重複的發送者 ID，將這些 ID 設為陣列的 key
+            $messages_from_id = array_fill_keys(array_unique(array_column($messages, 'from_id')), ["total" => 0, "to_ids" => []]);
+            
+            collect($messages)->each(function ($msg, $key) use (&$messages_from_id) {
+                // to_ids: 對方 ID 表
+                // to_id_stat: 各 ID 收到的訊息數統計
+                $messages_from_id[$msg['from_id']]["total"]++;
+                if(!in_array($msg['to_id'], $messages_from_id[$msg['from_id']]["to_ids"])) {
+                    $messages_from_id[$msg['from_id']]["to_ids"][] = $msg['to_id'];
+                    $messages_from_id[$msg['from_id']]["to_id_stat"][$msg['to_id']]["count"] = 1;
+                }
+                else {
+                    $messages_from_id[$msg['from_id']]["to_id_stat"][$msg['to_id']]["count"]++;
+                }
+            });
 
+            // 曾經有發訊息給超過 10 個男會員
+            $filtered = collect($messages_from_id)->filter(function ($from_stat, $key) {
+                return count($from_stat["to_ids"]) > 10;
+            });
 
-
-
-            return view('admin.stat_test');
+            return $filtered;
         });
 
         Route::get("jsfp_pro_validation", function() {
