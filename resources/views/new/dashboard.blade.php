2c5c87c573fb5350dcf817e09e280aa59cde587c
simon821010 <simon821010@gmail.com>
2022-07-04 11:19:36 +0800

    2022-07-04-1119-顯示修正

--- resources/views/new/dashboard.blade.php
+++ resources/views/new/dashboard.blade.php
@@ -773,18 +773,18 @@ dt span.engroup_type_title {display:inline-block;width:10%;white-space:nowrap;}
                     --}}
                     <dt>
                         <span>每月預算</span>
-                        <span>
-                            <input id="budget_per_month_min" name="budget_per_month_min" type="number" class="select_xx01 se_zlman left"  placeholder="請輸入最低金額" value="{{$umeta->budget_per_month_min}}" data-parsley-errors-messages-disabled onchange="c5('每月預算是非常重要的欄位，請確實填寫，如果被檢舉浮報或未給，查證屬實。將會有”預算不實”的標籤。')">
+                        <span style="text-align:center;">
+                            <input id="budget_per_month_min" name="budget_per_month_min" type="number" style="width:48%;" class="select_xx01 se_zlman left"  placeholder="請輸入最低金額" value="{{$umeta->budget_per_month_min}}" data-parsley-errors-messages-disabled onchange="c5('每月預算是非常重要的欄位，請確實填寫，如果被檢舉浮報或未給，查證屬實。將會有”預算不實”的標籤。')">
                             ~
-                            <input id="budget_per_month_max" name="budget_per_month_max" type="number" class="select_xx01 se_zlman right"  placeholder="請輸入最高金額" value="{{$umeta->budget_per_month_max}}" data-parsley-errors-messages-disabled>
+                            <input id="budget_per_month_max" name="budget_per_month_max" type="number" style="width:48%;" class="select_xx01 se_zlman right"  placeholder="請輸入最高金額" value="{{$umeta->budget_per_month_max}}" data-parsley-errors-messages-disabled>
                         </span>
                     </dt>
                     <dt>
                         <span>車馬費預算</span>
-                        <span>
-                            <input id="transport_fare_min" name="transport_fare_min" type="number" class="select_xx01 se_zlman left"  placeholder="請輸入最低金額" value="{{$umeta->transport_fare_min}}" data-parsley-errors-messages-disabled onchange="c5('車馬費預算是非常重要的欄位，請確實填寫，如果被檢舉浮報或未給查證屬實。將會有”車馬費不實”的標籤。')">
+                        <span style="text-align:center;">
+                            <input id="transport_fare_min" name="transport_fare_min" type="number" style="width:48%;" class="select_xx01 se_zlman left"  placeholder="請輸入最低金額" value="{{$umeta->transport_fare_min}}" data-parsley-errors-messages-disabled onchange="c5('車馬費預算是非常重要的欄位，請確實填寫，如果被檢舉浮報或未給查證屬實。將會有”車馬費不實”的標籤。')">
                             ~
-                            <input id="transport_fare_max" name="transport_fare_max" type="number" class="select_xx01 se_zlman right"  placeholder="請輸入最高金額" value="{{$umeta->transport_fare_max}}" data-parsley-errors-messages-disabled>
+                            <input id="transport_fare_max" name="transport_fare_max" type="number" style="width:48%;" class="select_xx01 se_zlman right"  placeholder="請輸入最高金額" value="{{$umeta->transport_fare_max}}" data-parsley-errors-messages-disabled>
                         </span>
                     </dt>
                 </div>
