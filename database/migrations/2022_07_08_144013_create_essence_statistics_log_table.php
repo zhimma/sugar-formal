101b99ef29db09275774e55199016e12cd47cded
LZong <lzong.tw@gmail.com>
2022-07-14 16:18:41 +0800

    Migration bug fixed.

--- database/migrations/2022_07_08_144013_create_essence_statistics_log_table.php
+++ database/migrations/2022_07_08_144013_create_essence_statistics_log_table.php
@@ -18,7 +18,7 @@ class CreateEssenceStatisticsLogTable extends Migration
                 $table->id();
                 $table->integer('user_id');
                 $table->integer('essence_posts_id');
-                $table->string('message_client_id',50)->nullable()->after('essence_posts_id');
+                $table->string('message_client_id',50)->nullable();
                 $table->timestamp('message_send_time');
                 $table->timestamps();
             });
