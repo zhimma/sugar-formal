90a776474765e30f90f93e1c18bb6c98d036e1c4
simon821010 <simon821010@gmail.com>
2022-07-20 09:48:36 +0800

    新增欄位

---
+++ database/migrations/2022_07_19_173306_add_column_of_user_meta.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnOfUserMeta extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_meta', function (Blueprint $table) {
+            $table->string('family_situation')->nullable()->after('smoking');
+            $table->string('available_time')->nullable()->after('smoking');
+            $table->string('body_touch')->nullable()->after('smoking');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_meta', function (Blueprint $table) {
+            $table->dropColumn('is_write');
+            $table->dropColumn('available_time');
+            $table->dropColumn('body_touch');
+        });
+    }
+}
