b75ddd89d133885838c40290022a89402f496bab
simon821010 <simon821010@gmail.com>
2022-08-16 00:07:04 +0800

    將後台管理員視訊頁面vue及前台視訊頁面vue分離

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,7 @@ window.Vue = require('vue').default;
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component("video-chat", require("./components/VideoChat.vue").default);
+Vue.component("video-verify-user", require("./components/VideoVerifyUser.vue").default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
