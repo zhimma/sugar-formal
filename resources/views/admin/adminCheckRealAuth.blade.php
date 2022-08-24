1ea2cb384002b2dd7b2a44b1c7f5c29da73d0864
yue <yueitng1214@gmail.com>
2022-08-24 16:27:29 +0800

    站長審核後台調整

--- resources/views/admin/adminCheckRealAuth.blade.php
+++ resources/views/admin/adminCheckRealAuth.blade.php
@@ -5,6 +5,7 @@
         <thead>
             <tr>
                 <th scope="col">email</th>
+                <th scope="col">暱稱</th>
                 <th scope="col">申請時間</th>
                 <th scope="col">審核時間</th>
                 <th scope="col">申請類型</th>
@@ -14,11 +15,15 @@
         </thead>
         <tbody>
         @foreach($row_list as $row)
-            <tr>
+            <tr style="border-top: solid;">
                 <td scope="row"  {!!$service->getRowspanAttr($row->real_auth_modify_item_group_modify_with_trashed->count())!!}>
                     <a href="{{route('users/advInfo',['id'=>$row->id])}}" target="_blank">
                         {{$row->email}}
                     </a>
+                    <a class="btn btn-success" href="/admin/users/message/to/{{ $row->id }}" target="_blank;">站長對話</a>
+                </td>
+                <td >
+                    {{$row->name}}
                 </td>
                 <td >
                     {{$row->real_auth_modify_item_group_modify_with_trashed->first()->real_auth_user_modify_with_trashed->created_at??null}}
@@ -38,7 +43,17 @@
                 </td>               
             </tr>
             @foreach($row->real_auth_modify_item_group_modify_with_trashed->slice(1) as $modify)
-            <tr>    
+            @php
+                $temp_user_id='';
+                $now_user_id=$row->id;
+                if($temp_user_id!==$now_user_id){
+                    $temp_user_id=$now_user_id;
+                }
+            @endphp
+            <tr @if($temp_user_id==$row->id) style="border-bottom: solid;" @endif>
+                <td >
+                    {{$row->name}}
+                </td>
                 <td>{{$modify->real_auth_user_modify_with_trashed->created_at}}</td>
                 <td>
                     {{$service->handleNullWord($modify->real_auth_user_modify_with_trashed->status_at,'尚未審核')}}
