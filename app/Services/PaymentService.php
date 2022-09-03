0f7976ec668343cf6243f8b8a2d6d6e76f5493ba
simon821010 <simon821010@gmail.com>
2022-09-03 18:03:27 +0800

    新增退費用migration及service

---
+++ app/Services/PaymentService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Order;
+use App\Models\ValueAddedService;
+
+class PaymentService
+{
+    public static function caculatesRefund()
+    {
+
+    }
+}
