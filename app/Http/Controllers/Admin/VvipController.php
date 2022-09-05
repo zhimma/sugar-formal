06c430989a234b969c52d705043005abe4f7f78d
simon821010 <simon821010@gmail.com>
2022-09-05 15:58:57 +0800

    待取消名單邏輯修正

--- app/Http/Controllers/Admin/VvipController.php
+++ app/Http/Controllers/Admin/VvipController.php
@@ -36,7 +36,7 @@ class VvipController extends \App\Http\Controllers\BaseController
 
     public function viewVvipCancellationList()
     {
-        $VVIPplanA = VvipApplication::where([['plan', 'VVIP_A'], ['created_at', '>', now()->addDays(3)]])->orderBy('id', 'desc')->get();
+        $VVIPplanA = VvipApplication::where([['plan', 'VVIP_A'], ['created_at', '<', now()->subDays(3)]])->orderBy('id', 'desc')->get();
         $VVIPplanA->each(function ($item) {
             if($item->user->VvipMargin?->balance < 20000) {
                 [$refund, ] = PaymentService::calculatesRefund($item->user, 'vvip_without_remittance');
@@ -49,7 +49,7 @@ class VvipController extends \App\Http\Controllers\BaseController
             }
         });
         
-        $VVIPplanB = VvipApplication::where([['plan', 'VVIP_B'], ['created_at', '>', now()->addDays(3)]])->orderBy('id', 'desc')->get();
+        $VVIPplanB = VvipApplication::where([['plan', 'VVIP_B'], ['created_at', '<', now()->subDays(3)]])->orderBy('id', 'desc')->get();
         $VVIPplanB->each(function ($item) {
             if($item->user->VvipMargin?->balance < 50000) {
                 [$refund, ] = PaymentService::calculatesRefund($item->user, 'vvip_without_remittance');
