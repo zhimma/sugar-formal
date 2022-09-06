b5ccf1084335a5a7b17cc4f96b5f5c16fbfdae42
qinghuang <ppcs0520@yahoo.com.tw>
2022-09-06 23:47:56 +0800

    20220906  修改VIP搜索不需要可以重複縣市區域

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4037,25 +4037,25 @@ class PagesController extends BaseController
         $county_array = ['county', 'county2', 'county3', 'county4', 'county5'];
         foreach ($input as $key =>$value){
             if(isset($input['county5']) && $key =='county5'){
-                if($input['county4'] == $input['county5'] || $input['county3'] == $input['county5'] || $input['county2'] == $input['county5'] || $input['county'] == $input['county5']){
+                if(($input['county4'] == $input['county5'] || $input['county3'] == $input['county5'] || $input['county2'] == $input['county5'] || $input['county'] == $input['county5']) && ($input['county4'] ==$input['district5'] || $input['district3'] == $input['district5'] || $input['district2'] == $input['district5'] || $input['district'] == $input['district5'])){
                     request()->county5 = null;
                     $input['county5'] = null;
                 }
             }
             if(isset($input['county4']) && $key =='county4'){
-                if($input['county3'] == $input['county4'] || $input['county2'] == $input['county4'] || $input['county'] == $input['county4']){
+                if(($input['county3'] == $input['county4'] || $input['county2'] == $input['county4'] || $input['county'] == $input['county4']) && ($input['district3'] == $input['district4'] || $input['district2'] == $input['district4'] || $input['district'] == $input['district4'])){
                     request()->county4 = null;
                     $input['county4'] = null;
                 }
             }
             if(isset($input['county3']) && $key =='county3'){
-                if($input['county2'] == $input['county3'] || $input['county'] == $input['county3']){
+                if(($input['county2'] == $input['county3'] || $input['county'] == $input['county3']) && ($input['district2'] == $input['district3'] || $input['district'] == $input['district3'])){
                     request()->county3 = null;
                     $input['county3'] = null;
                 }
             }
             if(isset($input['county2']) && $key =='county2'){
-                if($input['county'] == $input['county2']){
+                if(($input['county'] == $input['county2']) && ($input['district'] == $input['district2'])){
                     request()->county2 = null;
                     $input['county2'] = null;
                 }
