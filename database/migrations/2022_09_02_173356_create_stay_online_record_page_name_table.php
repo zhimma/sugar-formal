5521930f7eeaa13f4ee14a8952949df61d56fb4c
LZong <lzong.tw@gmail.com>
2022-09-07 23:34:36 +0800

    Migration bug modified.

--- database/migrations/2022_09_02_173356_create_stay_online_record_page_name_table.php
+++ database/migrations/2022_09_02_173356_create_stay_online_record_page_name_table.php
@@ -18,7 +18,8 @@ class CreateStayOnlineRecordPageNameTable extends Migration
         if (!Schema::hasTable('stay_online_reocrd_page_name')) {
             Schema::create('stay_online_record_page_name', function (Blueprint $table) {
                 $table->id();
-                $table->text('url')->nullable()->index();
+                $table->text('url')->nullable();
+                DB::unprepared('ALTER TABLE stay_online_reocrd_page_name ADD INDEX url url');
                 $table->string('name', 255)->nullable();
                 $table->timestamp('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
                 $table->timestamp('updated_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
