10050138491e7da4edc84e80c487def35d9d1e1b
LZong <lzong.tw@gmail.com>
2022-09-08 13:09:26 +0800

    20220906 不同欄位間跳行

--- resources/views/admin/users/userPicturesSimilar.blade.php
+++ resources/views/admin/users/userPicturesSimilar.blade.php
@@ -167,6 +167,7 @@
                                             關於我: 
                                             <textarea type="text" class="form-control form-control-sm form-control-plaintext mr-sm-2" name="about">{{ $user->meta_()->about }}</textarea>
                                         </span>
+                                        <br>
                                         <span>
                                             期待的約會模式: 
                                             <textarea type="text" class="form-control form-control-sm form-control-plaintext mr-sm-2" name="style">{{ $user->meta_()->style }}</textarea>
