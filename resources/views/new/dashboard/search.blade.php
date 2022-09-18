5434464d0aa4c2d3b7a934529221ae99b69228c3
LZong <lzong.tw@gmail.com>
2022-09-18 23:03:52 +0800

    Search strange situation fixed.

--- resources/views/new/dashboard/search.blade.php
+++ resources/views/new/dashboard/search.blade.php
@@ -615,7 +615,7 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
                     $icc = 1;
                     $userIsVip = ($user->isVIP() || $user->isVVIP());
 
-                    if(isset($_GET['page'])){
+                    if(isset($_GET['page']) && is_numeric($_GET['page'])){
                         $page = $_GET['page'];
                         $page_pre = $page-1>0 ?$page-1: 1;
                         $page_next = $page+1;
