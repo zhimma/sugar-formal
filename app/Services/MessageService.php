6796d0acf77681e25122ab2276adccaba952dd64
Qq by LZong <lzong.tw@gmail.com>
2022-09-22 19:25:22 +0800

    補上漏掉的檔案

--- app/Services/MessageService.php
+++ app/Services/MessageService.php
@@ -14,4 +14,18 @@ class MessageService
     {
     	$this->message = $message;
     }
+
+    public function setMessageHandling($messageId,$handleStatus){
+        return $this->message->where('id',$messageId)->update(['handle'=>$handleStatus]);
+    }
+
+    public function setMessageHandlingBySenderId($userid){
+        return $this->message->where('from_id',$userid)->update(['handle'=>0]);
+    }
+
+    public function getMessageById($id){
+        return $this->message->where([
+            'id'     => $id,
+        ])->first();
+    }
 }
