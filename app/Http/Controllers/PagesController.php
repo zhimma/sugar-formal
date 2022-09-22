504128d244fb57077709283c3c5ce47da5a9c06b
simon821010 <simon821010@gmail.com>
2022-09-22 14:45:52 +0800

    留言板連結修正

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8879,7 +8879,12 @@ class PagesController extends BaseController
                 $ssrData .='<div class="liuyan_nlist">';
                 $ssrData .='<ul>';
                 $ssrData .='<li>';
-                $ssrData .='<a href="/dashboard/viewuser/'.$list->uid.'">';
+                if($msgUser->isVVIP()){
+                    $ssrData .='<a href="/dashboard/viewuser_vvip/'.$list->uid.'">';
+                }
+                else{
+                    $ssrData .='<a href="/dashboard/viewuser/'.$list->uid.'">';
+                }
                 $ssrData .='<div class="liuyan_img"><img class="hycov '.$is_blur.'" src="'.$umpic.'"></div>';
                 $ssrData .='</a>';
                 $ssrData .='<a href="/MessageBoard/post_detail/'. $list->mid.'">';
@@ -9007,7 +9012,12 @@ class PagesController extends BaseController
                 $ssrData .='<div class="liuyan_nlist">';
                 $ssrData .='<ul>';
                 $ssrData .='<li>';
-                $ssrData .='<a href="/dashboard/viewuser/'.$list->uid.'">';
+                if($msgUser->isVVIP()){
+                    $ssrData .='<a href="/dashboard/viewuser_vvip/'.$list->uid.'">';
+                }
+                else{
+                    $ssrData .='<a href="/dashboard/viewuser/'.$list->uid.'">';
+                }
                 $ssrData .='<div class="liuyan_img"><img class="hycov '.$is_blur.'" src="'.$umpic.'"></div>';
                 $ssrData .='</a>';
                 $ssrData .='<a href="/MessageBoard/post_detail/'. $list->mid.'">';
