039b40d4fda7f6d8a0da9561024c91cc70519b86
simon821010 <simon821010@gmail.com>
2022-10-19 14:34:59 +0800

    名人美顏通過時藍勾勾照常顯示

--- resources/views/new/dashboard/viewuser.blade.php
+++ resources/views/new/dashboard/viewuser.blade.php
@@ -554,11 +554,8 @@
                             <ul @if(!$user->isVip() && !$user->isVVIP())style="margin-top: -5px;"@endif class="@if(!$user->isVip() && !$user->isVVIP()) not_vip  @endif">
                                 @php
                                     $blue_tick = 0;
-                                    if(!$rap_service->isPassedByAuthTypeId(2) && !$rap_service->isPassedByAuthTypeId(3))
-                                    {
-                                        if($to->isAdvanceAuth()){$blue_tick = $blue_tick + 1;}
-                                        if($rap_service->riseByUserEntry($to)->isPassedByAuthTypeId(1)){$blue_tick = $blue_tick + 1;}
-                                    }
+                                    if($to->isAdvanceAuth()){$blue_tick = $blue_tick + 1;}
+                                    if($rap_service->riseByUserEntry($to)->isPassedByAuthTypeId(1)){$blue_tick = $blue_tick + 1;}
                                 @endphp
                                 @if($blue_tick == 2)
                                     <img src="/new/images/zz_zss.png" style="border-radius: 100px; box-shadow:1px 2px 10px rgba(77,152,252,1); height:20px; margin-top:6px;">
