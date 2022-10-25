b35cd591da555677ddabdd48bbbadd12f7f56cf9
LZong <lzong.tw@gmail.com>
2022-10-26 01:30:47 +0800

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -94,10 +94,7 @@ class Kernel extends ConsoleKernel
             //$schedule->command('EncodeImagesForCompare')->timezone('Asia/Taipei')->dailyAt('02:01');
             //$schedule->command('queue:work --queue=compare_images --daemon --sleep=3 --tries=3 --delay=3  --timeout=0')->timezone('Asia/Taipei')->everyFiveMinutes()->between('02:00', '12:00');
             $schedule->command('CompareImages')->timezone('Asia/Taipei')->dailyAt('08:00');       
-            $schedule->command('CompareImages  --dsort')->timezone('Asia/Taipei')->everyTenMinutes();//->between('02:00', '12:00');   
-
-            //每小時檢查登入使用者
-            $schedule->command('UserLogin')->timezone('Asia/Taipei')->hourly();         
+            $schedule->command('CompareImages  --dsort')->timezone('Asia/Taipei')->everyTenMinutes();//->between('02:00', '12:00');     
         }
         if(app()->isProduction() || app()->isLocal()){
             
