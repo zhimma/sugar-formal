3e7b669375e96eefc5e0b2f1dd1a7be2c587f6c1
simon821010 <simon821010@gmail.com>
2022-11-13 22:50:48 +0800

    新增seeder測試

---
+++ database/seeds/CanMessageSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+use App\Models\MessageRoomUserXref;
+
+use App\Models\MessageRoom;
+
+use App\Models\Message;
+
+class CanMessageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $to_users = User::where('engroup', 2)->inRandomOrder()->take(10)->get();
+        $from_user_id_array = [15598,15601];
+        foreach($from_user_id_array as $from_user_id)
+        {
+            foreach($to_users as $to_user)
+            {
+                $room_id = 0;
+                $room = MessageRoomUserXref::select('message_room_user_xrefs.room_id as r_id')->leftJoin('message_room_user_xrefs as sub_table', 'message_room_user_xrefs.room_id', '=', 'sub_table.room_id')
+                                    ->where('message_room_user_xrefs.user_id', $from_user_id)
+                                    ->where('sub_table.user_id', $to_user->id)
+                                    ->first();
+                if($room ?? false)
+                {
+                    $room_id = $room->r_id;
+                }
+                else
+                {
+                    $new_room = new MessageRoom();
+                    $new_room->save();
+                    $room_id = $new_room->id;
+                    MessageRoomUserXref::create([
+                        'room_id' => $room_id,
+                        'user_id' => $from_user_id
+                    ]);
+                    MessageRoomUserXref::create([
+                        'room_id' => $room_id,
+                        'user_id' => $to_user->id
+                    ]);
+                }
+                Message::create([
+                    'room_id' => $room_id,
+                    'to_id' => $to_user->id,
+                    'from_id' => $from_user_id,
+                    'content' => '哈囉你好嗎?'
+                ]);
+            }
+        }
+    }
+}
