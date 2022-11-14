58c83439c8886dcfc5e0d9b1c79e8817e1340348
simon821010 <simon821010@gmail.com>
2022-11-14 14:49:25 +0800

    test

--- resources/views/admin/users/advInfo.blade.php
+++ resources/views/admin/users/advInfo.blade.php
@@ -3264,15 +3264,40 @@ function show_re_content(id){
                             name_color = 'color: #5867DD;';
                         }
 
-                        console.log(value.u_id);
-                        console.log(data.users_data[value.u_id]);
+                        user_icon = '';
+                        if(data.users_data[value.u_id]['vip']){
+                            if(data.users_data[value.u_id]['vip'] == 'diamond_black'){
+                                user_icon += '<img src="/img/diamond_black.png" style="height: 16px;width: 16px;">';
+                            }
+                            else{
+                                for(i = 0; i < data.users_data[value.u_id]['vip']; i++){
+                                    user_icon += '<img src="/img/diamond.png" style="height: 16px;width: 16px;">';
+                                }
+                            }
+                        }
+
+                        for(i = 0; i < data.users_data[value.u_id]['tipcount']; $i++){
+                            user_icon += '👍';
+                        }
+
+                        if(value.banned_id){
+                            if(value.banned_expire_date){
+                                let exp_date = new Date(value.banned_expire_date);
+                                let now_date = Date.now();
+                                let idays = parseInt(Math.abs(exp_date - now_date) / 1000 / 60 / 60 / 24);
+                                user_icon += '(' + idays + '天)';
+                            }
+                            else{
+                                user_icon += '(永久)';
+                            }
+                        }
 
                         $('#message_room_detail_' + data.room_id).append(
                             '<tr>'+
                                 '<td style="text-align: right;">'+
                                     '<a href="' + data.message_href + '" target="_blank">'+
                                         '<p style="margin-bottom:0px;'+ name_color +'">'+
-                                            value.name +
+                                            value.name + user_icon +
                                         '</p>'+
                                     '</a>'+
                                 '</td>'+
