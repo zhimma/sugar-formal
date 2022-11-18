8b016bcb087ba5aa2d0a56e55c3ef4c949b400cd
simon821010 <simon821010@gmail.com>
2022-11-18 15:29:37 +0800

    加入可疑名單

--- app/Console/Commands/Step2CheckExtendRecheck.php
+++ app/Console/Commands/Step2CheckExtendRecheck.php
@@ -4,6 +4,18 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
+use App\Models\BackendUserDetails;
+
+use App\Models\User;
+
+use App\Models\Message;
+
+use App\Models\SuspiciousUser;
+
+use Illuminate\Support\Facades\Log;
+
+use Carbon\Carbon;
+
 class Step2CheckExtendRecheck extends Command
 {
     /**
@@ -11,14 +23,14 @@ class Step2CheckExtendRecheck extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'Step2CheckExtendRecheck';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = '等待更多資料名單檢查';
 
     /**
      * Create a new command instance.
@@ -37,6 +49,42 @@ class Step2CheckExtendRecheck extends Command
      */
     public function handle()
     {
-        return 0;
+        $user_list = [];
+        $check_list = BackendUserDetails::with('check_extend_admin_action_log')
+                                        ->with('user')
+                                        ->select('user_id')
+                                        ->where('user_check_step2_wait_login_times', '>', 0)
+                                        ->get();
+        foreach($check_list as $check)
+        {
+            //如果最後登入時間早於等待檢查開始的時間則跳過
+            $check_start_time = $check->check_extend_admin_action_log->first() ? $check->check_extend_admin_action_log->first()->created_at : false;
+            if($check_start_time)
+            {
+                if($check_start_time > $check->user->last_login)
+                {
+                    continue;
+                }
+            }
+            $user_list[$check->user->id]['check_data'] = $check;
+            $user_list[$check->user->id]['check_start_time'] = $check_start_time;
+        }
+        $message = Message::select('from_id','created_at')->whereIn('from_id', array_keys($user_list))->get();
+        
+        foreach($user_list as $user_id => $check_data)
+        {
+            $user_message_count = $message->where('from_id', $user_id)->where('created_at', '>', $check_data['check_start_time'])->count();
+            if($user_message_count > 5)
+            {
+                SuspiciousUser::where('user_id', $user_id)->delete();
+                $suspicious_user = new SuspiciousUser();
+                $suspicious_user->admin_id = 0;
+                //$suspicious_user->admin_id = $check_data['check_data']->check_extend_admin_action_log->first() ? $check_data['check_data']->check_extend_admin_action_log->first()->operator : 0;
+                $suspicious_user->user_id = $user_id;
+                $suspicious_user->reason = '(系統自動新增)等待更多資料時與超過五個會員對話';
+                $suspicious_user->save();
+            }
+        }
+        
     }
 }
