23dd2d96eefe5d189e45fdc980e71cfcafd6ef70
simon821010 <simon821010@gmail.com>
2022-11-24 13:59:26 +0800

    繼承舊紀錄

--- database/migrations/2022_11_24_105732_remove_user_check_step2_wait_login_times.php
+++ database/migrations/2022_11_24_105732_remove_user_check_step2_wait_login_times.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\BackendUserDetails;
 
 class RemoveUserCheckStep2WaitLoginTimes extends Migration
 {
@@ -14,8 +15,13 @@ class RemoveUserCheckStep2WaitLoginTimes extends Migration
     public function up()
     {
         Schema::table('backend_user_details', function($table) {
-            $table->dropColumn('user_check_step2_wait_login_times');
             $table->boolean('is_waiting_for_more_data')->default(0)->after('user_id');
+        });
+
+        BackendUserDetails::where('user_check_step2_wait_login_times', '!=', 0)->update(['is_waiting_for_more_data' => 1]);
+
+        Schema::table('backend_user_details', function($table) {
+            $table->dropColumn('user_check_step2_wait_login_times');
         });
     }
 
