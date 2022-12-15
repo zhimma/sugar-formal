d8cbd41b2c678ce6ac218714a6aaada2c15b0add
LZong <lzong.tw@gmail.com>
2022-12-15 14:45:55 +0800

    VIP check bug fixed.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -189,7 +189,7 @@ class Kernel extends ConsoleKernel
     }
 
     protected function checkECPayVip(){
-       $vipDatas = \App\Models\Vip::where(['business_id' => '3137610', 'active' => 1])->get();
+       $vipDatas = \App\Models\Vip::where(['free' => 0, 'active' => 1])->get();
         // $vipDatas = \App\Models\Vip::where('business_id','3137610')->where('order_id','like','SG%')->get();
         foreach ($vipDatas as $vipData){
             \App\Jobs\CheckECpay::dispatch($vipData);
