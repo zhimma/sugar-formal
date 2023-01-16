25d4ebe863b9724ef0603e4e439e2a436c246209
LZong <lzong.tw@gmail.com>
2023-01-16 16:21:26 +0800

    Exception handler.

--- resources/views/dashboard/messageBoard_detail.blade.php
+++ resources/views/dashboard/messageBoard_detail.blade.php
@@ -67,7 +67,7 @@
 					<div class="shou">
 						<span>留言板詳情</span><font>Wishing Board</font>
 						@php
-							$previous_url=str_contains($_SERVER['HTTP_REFERER'], 'viewuser') ? $_SERVER['HTTP_REFERER'] : '/MessageBoard/showList';
+							$previous_url = isset($_SERVER['HTTP_REFERER']) ? (str_contains($_SERVER['HTTP_REFERER'], 'viewuser') ? $_SERVER['HTTP_REFERER'] : '/MessageBoard/showList') : '/MessageBoard/showList';
 							if(str_contains($_SERVER['HTTP_REFERER'], 'viewuser/')){
 								$previous_url= $previous_url.'#messageBoard_'.$postDetail->mid;
 								session()->put('viewuser_page_position', 'messageBoard_'.$postDetail->mid);
