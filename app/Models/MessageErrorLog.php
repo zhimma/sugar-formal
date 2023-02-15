c3e369046c511b70a81d7b1e1432872db87c26c8
wayjen <iih1n9beb-els@yahoo.com>
2023-02-15 15:54:41 +0800

    紀錄會員傳訊的傳送失敗錯誤訊息：補model和migration

---
+++ app/Models/MessageErrorLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MessageErrorLog extends Model
+{
+    //
+    protected $table = 'message_error_log';
+
+    protected $guarded = ['id'];
+}
