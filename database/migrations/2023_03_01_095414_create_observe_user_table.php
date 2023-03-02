67f3a0bc37818a170f9d8fa0be4d865c670ad224
yue <yueitng1214@gmail.com>
2023-03-02 14:09:05 +0800

    後台新增觀察名單功能＆列表

---
+++ database/migrations/2023_03_01_095414_create_observe_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateObserveUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('observe_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('reason', 255)->nullable();
+            $table->integer('admin_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('observe_user');
+    }
+}
