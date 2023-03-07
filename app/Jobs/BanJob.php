f5db3c7701970d7db9f5db9012ad25a3a2649546
LZong <lzong.tw@gmail.com>
2023-03-08 00:29:37 +0800

    Trackers added.

--- app/Jobs/BanJob.php
+++ app/Jobs/BanJob.php
@@ -13,6 +13,7 @@ use App\Models\SimpleTables\banned_users;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use App\Models\BannedUsersImplicitly;
+use App\Models\User;
 use Illuminate\Support\Facades\Log;
 
 class BanJob implements ShouldQueue
@@ -38,6 +39,10 @@ class BanJob implements ShouldQueue
 
     public function handle()
     {
+        if (!$this->user) {
+            logger("Ban job failed on user {$this->uid}, user not found, try to find user again.");
+            $this->user = User::find($this->uid);
+        }
         if($this->user ?? false)
         {
             Log::info("start_jobs_BanJob at " . now() . ", user {$this->user->id}, ban set {$this->ban_set->id}");
@@ -45,26 +50,30 @@ class BanJob implements ShouldQueue
                 logger("Ban job failed on user {$this->user->id}, no ban set id, try to display ban type: " . $this->ban_set->set_ban);
             }
             if (!$this->ban_set->set_ban) {
-                logger("Ban job failed on user {$this->user->id}, set id: {$this->ban_set->set_ban}.");
+                logger("Ban job failed on user {$this->user->id}, no ban type, set id: {$this->ban_set->set_ban}.");
             }
             $that = $this;
             $user_had_been_banned = DB::connection($this->connection)->table('banned_users')->where('member_id', $this->uid)->get()->first();
             $user_had_been_implicitly_banned = DB::connection($this->connection)->table('banned_users_implicitly')->where('target', $this->uid)->get()->first();
             $user_had_been_warned = DB::connection($this->connection)->table('warned_users')->where('member_id', $this->uid)->get()->first();
+            logger("Ban job on user {$this->user->id}, ban set {$this->ban_set->id}, user_had_been_banned: " . ($user_had_been_banned ? 'true' : 'false') . ", user_had_been_implicitly_banned: " . ($user_had_been_implicitly_banned ? 'true' : 'false') . ", user_had_been_warned: " . ($user_had_been_warned ? 'true' : 'false'));
             if($this->ban_set->set_ban == 1 && !$user_had_been_banned)
             {
                 //直接封鎖
                 $userBanned = new banned_users;
                 if($this->user->engroup==2) {
-                if(!($this->user->advance_auth_status ?? null)) {
-                    $userBanned->adv_auth=1;
-                }
-                else {
+                    logger("User {$this->user->id} is female.");
+                    if(!($this->user->advance_auth_status ?? null)) {
+                        $userBanned->adv_auth=1;
+                        logger("Ban job set adv auth on user {$this->user->id}, ban set {$this->ban_set->id}");
+                    }
+                    else {
                         $userBanned = null;
                         logger("Ban job skipped on user {$this->user->id}, user is already adv authed.");
                     }
                 }                         
                 if($userBanned) {
+                    logger("Ban job set ban on user {$this->user->id} initiated, ban set {$this->ban_set->id}");
                     $userBanned->member_id = $this->uid;
                     $userBanned->reason = "系統原因(".$this->ban_set->id.")";
                     $userBanned->save();
@@ -80,19 +89,21 @@ class BanJob implements ShouldQueue
                 switch($this->type)
                 {
                     case 'profile':
-                        $Line = 79;
+                        $Line = 92;
                         break;
                     
                     case 'message':
-                        $Line = 124;
+                        $Line = 96;
                         break;
                 }
+                logger("Implicit banned user {$this->user->id} initiated, ban set {$this->ban_set->id}, line {$Line}");
                 DB::connection('mysql')->table('banned_users_implicitly')->insert(['fp' => 'Line ' . $Line . ', BannedInUserInfo, ban_set ID: ' . $this->ban_set->id . ', content: ' . $this->ban_set->content, 'user_id' => 0, 'target' => $this->uid]);
                 logger("Implicit banned user {$this->user->id}, ban set {$this->ban_set->id}");
             }
             elseif($this->ban_set->set_ban == 3 && !$user_had_been_warned)
             {
                 //警示會員
+                logger("Warned user {$this->user->id} initiated, ban set {$this->ban_set->id}");
                 $userWarned = new warned_users;
                 $userWarned->member_id = $this->uid;
                 $userWarned->reason = "系統原因(".$this->ban_set->id.")";
@@ -108,8 +119,14 @@ class BanJob implements ShouldQueue
                 // UserMeta::where('user_id', $this->uid)->update(['isWarned' => 1]);
                 logger("Warned user {$this->user->id}, ban set {$this->ban_set->id}");
             }
+            else {
+                logger("Ban job failed on user {$this->user->id}, ban set {$this->ban_set->id}, user_had_been_banned: " . ($user_had_been_banned ? 'true' : 'false') . ", user_had_been_implicitly_banned: " . ($user_had_been_implicitly_banned ? 'true' : 'false') . ", user_had_been_warned: " . ($user_had_been_warned ? 'true' : 'false'));
+            }
             //sleep(90);
             Log::info("end_jobs_BanJob at " . now() . ", user {$this->user->id}, ban set {$this->ban_set->id}");
+        }
+        else {
+            logger("Ban job failed on user {$this->uid}, user not found.");
         }
         
         return 0;
