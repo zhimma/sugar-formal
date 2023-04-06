07cbd1b71826ff4246a6ae797a417a87e97b8ad4
simon821010 <simon821010@gmail.com>
2023-04-06 15:21:31 +0800

    修正

--- resources/views/new/dashboard/chat.blade.php
+++ resources/views/new/dashboard/chat.blade.php
@@ -3211,11 +3211,10 @@ is_truth_icon_pic.src="{{asset('/new/images/zz_zt2.png')}}";
         @if($user->backend_user_details->first()->is_need_video_verify ?? false)
             @if($user->warned_users->video_auth ?? false)
                 @if($user->backend_user_details->first()->is_need_reverify ?? false)
-                    if(window.sessionStorage.getItem('has_need_reverify') != true){
+                    @if($user->advance_auth_status == 0 && $user->meta->phone == '')
                         c5('您上一次視訊驗證失敗，請重新進行手機驗證與email驗證，若有問題請與站長聯絡');
-                        window.sessionStorage.setItem('has_need_reverify', true);
                         location.href = '{{route("hint_to_video_record_verify_reverify")}}';
-                    }
+                    @endif
                 @else
                     @switch($user->backend_user_details->first()->video_verify_fail_count)
                         @case(0)
