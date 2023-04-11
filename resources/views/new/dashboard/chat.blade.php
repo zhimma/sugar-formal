96663d8171e8fe26c0123f3a4d97c5d68cd4308a
simon821010 <simon821010@gmail.com>
2023-04-11 18:33:33 +0800

    修改

--- resources/views/new/dashboard/chat.blade.php
+++ resources/views/new/dashboard/chat.blade.php
@@ -3220,7 +3220,7 @@ is_truth_icon_pic.src="{{asset('/new/images/zz_zt2.png')}}";
     $(function() {
         @if($user->backend_user_details->first()->is_need_video_verify ?? false)
             @if($user->warned_users->video_auth ?? false)
-                @if($user->backend_user_details->first()->is_need_reverify ?? false)
+                @if(($user->backend_user_details->first()->is_need_reverify ?? false) && $user->backend_user_details->first()->video_verify_fail_count <= 2)
                     @if($user->meta->phone ?? false && $user->meta->phone != '')
                         reverify_c5html('您上一次視訊驗證失敗，需要您重新進行進階驗證。您之前是使用手機驗證：{{$user->meta->phone}}，請確認是否重新驗證，按下確定後，即發送驗證碼至您原留手機，若有問題請與站長聯絡');
                         $('#reverify_button_field').html('<span><a id="reverify_enter" class="n_left">確定</a></span><span><a id="reverify_cancel" class="n_right" onclick="gmBtnNoReload()">取消</a></span>');
