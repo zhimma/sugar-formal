20eec1e9159e96ab22610b50d3eda367349ccb4f
simon821010 <simon821010@gmail.com>
2023-04-13 14:21:05 +0800

    修改popup次數

--- resources/views/new/dashboard/chat.blade.php
+++ resources/views/new/dashboard/chat.blade.php
@@ -3257,7 +3257,10 @@ is_truth_icon_pic.src="{{asset('/new/images/zz_zt2.png')}}";
                             @break
                     
                         @default
-                            c5html('您連續三次視訊驗證失敗，暫時停止視訊驗證，若有問題請與站長聯絡 <a href="https://lin.ee/rLqcCns"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="26" border="0" style="all: initial;all: unset;height: 26px; float: unset;vertical-align:middle !important;"></a>');
+                            if(!(sessionStorage.getItem('display_video_record_verify_fail'))){
+                                sessionStorage.setItem('display_video_record_verify_fail', true);
+                                c5html('您連續三次視訊驗證失敗，暫時停止視訊驗證，若有問題請與站長聯絡 <a href="https://lin.ee/rLqcCns"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="26" border="0" style="all: initial;all: unset;height: 26px; float: unset;vertical-align:middle !important;"></a>');
+                            }
                             @break
 
                     @endswitch
@@ -3283,7 +3286,10 @@ is_truth_icon_pic.src="{{asset('/new/images/zz_zt2.png')}}";
                         @break
                 
                     @default
-                        c5html('您連續三次視訊驗證失敗，暫時停止視訊驗證，若有問題請與站長聯絡 <a href="https://lin.ee/rLqcCns"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="26" border="0" style="all: initial;all: unset;height: 26px; float: unset;vertical-align:middle !important;"></a>');
+                        if(!(sessionStorage.getItem('display_video_record_verify_fail'))){
+                            sessionStorage.setItem('display_video_record_verify_fail', true);
+                            c5html('您連續三次視訊驗證失敗，暫時停止視訊驗證，若有問題請與站長聯絡 <a href="https://lin.ee/rLqcCns"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="26" border="0" style="all: initial;all: unset;height: 26px; float: unset;vertical-align:middle !important;"></a>');
+                        }
                         @break
 
                 @endswitch
