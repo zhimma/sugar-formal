0d70fd7ffb3231da2257ff0a0ec45e4287205c56
simon821010 <simon821010@gmail.com>
2023-04-13 16:14:36 +0800

    修改
    (cherry picked from commit 01587f226fef6e3bb6fa8dbb72b14dcc35819125)

--- resources/views/new/dashboard/account_manage.blade.php
+++ resources/views/new/dashboard/account_manage.blade.php
@@ -179,7 +179,13 @@
                 @if($user->video_verify_auth_status)
                     c5('已通過');
                 @else
-                    c5('已申請，站方會再跟您約驗證時間，再請注意來訊。');
+                    @if($user->warned_users->video_auth ?? false)
+                        c5('你好，您目前被站方警示，站方會再跟您約視訊驗證時間，再請注意來訊。');
+                    @elseif($user->warned_users->adv_auth ?? false)
+                        c5('你好，您目前被站方警示，請進行進階驗證。');
+                    @else
+                        c5('已申請，站方會再跟您約驗證時間，再請注意來訊。');
+                    @endif
                 @endif
             @endif
         });
