b8ac2f447c2ccc9d827aac46384146f838f4890d
simon821010 <simon821010@gmail.com>
2023-04-17 10:39:15 +0800

    歸零修正

--- app/Models/BackendUserDetails.php
+++ app/Models/BackendUserDetails.php
@@ -92,7 +92,9 @@ class BackendUserDetails extends Model
     {
         $backend_user_detail = BackendUserDetails::first_or_new($user_id);
         $backend_user_detail->video_verify_fail_count = 0;
+        $backend_user_detail->need_video_verify_date = Carbon::now();
         $backend_user_detail->login_times_after_need_video_verify_date = 0;
+        $backend_user_detail->is_need_reverify = 0;
         $backend_user_detail->save();
     }
 
