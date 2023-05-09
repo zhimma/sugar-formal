e68248c2770622f2d245066c8af2478a0d9b576e
simon821010 <simon821010@gmail.com>
2023-05-09 09:48:06 +0800

    修正進階驗證

--- resources/views/new/dashboard/account_manage.blade.php
+++ resources/views/new/dashboard/account_manage.blade.php
@@ -176,7 +176,7 @@
                         }
                     });
                 @else
-                    c5('尚未通過進階驗證');
+                    c5html("請先通過 進階驗證(<a href='/advance_auth'>點此前往</a>)");
                 @endif
             @else
                 @if($user->video_verify_auth_status)
@@ -189,7 +189,7 @@
                     @elseif($user->warned_users->video_auth ?? false)
                         c5('你好，您目前被站方警示，站方會再跟您約視訊驗證時間，再請注意來訊。');
                     @elseif($user->warned_users->adv_auth ?? false)
-                        c5('你好，您目前被站方警示，請進行進階驗證。');
+                        c5('你好，您目前被站方警示，請進行進階驗證(<a href="/advance_auth">點此前往</a>)。');
                     @else
                         c5('已申請，站方會再跟您約驗證時間，再請注意來訊。');
                     @endif
