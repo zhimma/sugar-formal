334264474db56ed7b8412ce8466d616b863ae530
LZong <lzong.tw@gmail.com>
2023-05-09 21:09:07 +0800

    Bug fixed.

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -7,7 +7,7 @@ if (! function_exists("db_config")) {
 
 if (! function_exists("user_allow_feature")) {
     function user_allow_feature($user) {
-        if (\App::environment("production")) {
+        if (!\App::environment("production")) {
             return true;
         }
         $allowList = config('app.newFeatureAllowList') ?? [];
