5a2decf0045994c70dbd4f677fb118576b81ac81
LZong <lzong.tw@gmail.com>
2023-07-22 02:08:09 +0800

    Remove color codes.

--- app/Console/Commands/AutoTestCommand.php
+++ app/Console/Commands/AutoTestCommand.php
@@ -17,6 +17,7 @@ class AutoTestCommand extends Command
         $lineNotify = new LineNotify;
         $lineNotify->sendLineNotifyMessage("開始自動測試，環境：" . \App::environment());
         $result = shell_exec("cd " . $root_path . " && sudo ./vendor/bin/pest 2>&1");
+        $result = preg_replace('#\\x1b[[][^A-Za-z]*[A-Za-z]#', '', $result);
         logger($result);
         \Sentry\captureMessage("auto test result: " . $result);
         $lineNotify = new LineNotify;
