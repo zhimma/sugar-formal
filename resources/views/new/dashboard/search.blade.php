afa7b7c5112fad7adacda97d34b1d11071c5c6f0
simon821010 <simon821010@gmail.com>
2023-08-21 17:47:31 +0800

    修正

--- resources/views/new/dashboard/search.blade.php
+++ resources/views/new/dashboard/search.blade.php
@@ -1636,6 +1636,9 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
             })
             ;
             //有舊選項時選擇舊選項
+            @php
+                $search_tag_selected_list = [];
+            @endphp
             @if(!empty($_POST["search_tag"]))
                 @php
                     $search_tag_selected_list = $_POST["search_tag"];
