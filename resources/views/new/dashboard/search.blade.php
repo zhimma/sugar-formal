374a5814ff96ed36ad6342f63768a131093daf71
simon821010 <simon821010@gmail.com>
2023-08-21 14:30:05 +0800

    年齡身高提示修改為popup

--- resources/views/new/dashboard/search.blade.php
+++ resources/views/new/dashboard/search.blade.php
@@ -238,9 +238,9 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
                                         <dt>
                                             <span>年齡範圍</span>
                                             <span style="display: inline-flex;">
-                                            <input class="select_xx06" name="agefrom" id="agefrom" type="number" min="18" max="80" value="@if(!empty($_POST['agefrom'])){{ $_POST['agefrom'] }}@elseif(!empty($_GET['agefrom'])){{$_GET['agefrom']}}@elseif(!empty(session()->get('search_page_key.agefrom'))){{ session()->get('search_page_key.agefrom')  }}@endif">
+                                            <input class="select_xx06" name="agefrom" id="agefrom" type="number" value="@if(!empty($_POST['agefrom'])){{ $_POST['agefrom'] }}@elseif(!empty($_GET['agefrom'])){{$_GET['agefrom']}}@elseif(!empty(session()->get('search_page_key.agefrom'))){{ session()->get('search_page_key.agefrom')  }}@endif">
                                             <div class="sew6">至</div>
-                                            <input class="select_xx06 right" name="ageto" id="ageto" type="number" min="18" max="80" value="@if(!empty($_POST['ageto'])){{$_POST['ageto'] }}@elseif(!empty($_GET['ageto'])){{$_GET['ageto']}}@elseif(!empty(session()->get('search_page_key.ageto'))){{ session()->get('search_page_key.ageto')  }}@endif">
+                                            <input class="select_xx06 right" name="ageto" id="ageto" type="number" value="@if(!empty($_POST['ageto'])){{$_POST['ageto'] }}@elseif(!empty($_GET['ageto'])){{$_GET['ageto']}}@elseif(!empty(session()->get('search_page_key.ageto'))){{ session()->get('search_page_key.ageto')  }}@endif">
                                             </span>
                                         </dt>
 
@@ -249,9 +249,9 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
                                             <span style="display: inline-flex;">
                                                 {{--150  <input id="heightRange" type="text" class="span2" name="heightRange" value="" data-slider-min="150" data-slider-max="180" data-slider-step="5" data-slider-value="[150,180]"/>  180--}}
                                                 {{--<input type="range" name="percent" min="50" max="100" value=@if(isset($_POST['percent']))"{{$_POST['percent']}}"@else"70"@endif class="form-control-range range" id="myRange">--}}
-                                            <input class="select_xx06" name="heightfrom" id="heightfrom" type="number" min="140" max="210" value="@if(!empty($_POST['heightfrom'])){{ $_POST['heightfrom'] }}@elseif(!empty($_GET['heightfrom'])){{$_GET['heightfrom']}}@elseif(!empty(session()->get('search_page_key.heightfrom'))){{ session()->get('search_page_key.heightfrom')  }}@endif">
+                                            <input class="select_xx06" name="heightfrom" id="heightfrom" type="number" value="@if(!empty($_POST['heightfrom'])){{ $_POST['heightfrom'] }}@elseif(!empty($_GET['heightfrom'])){{$_GET['heightfrom']}}@elseif(!empty(session()->get('search_page_key.heightfrom'))){{ session()->get('search_page_key.heightfrom')  }}@endif">
                                             <div class="sew6">至</div>
-                                            <input class="select_xx06 right" name="heightto" id="heightto" type="number" min="140" max="210" value="@if(!empty($_POST['heightto'])){{$_POST['heightto'] }}@elseif(!empty($_GET['heightto'])){{$_GET['heightto']}}@elseif(!empty(session()->get('search_page_key.heightto'))){{ session()->get('search_page_key.heightto')  }}@endif">
+                                            <input class="select_xx06 right" name="heightto" id="heightto" type="number" value="@if(!empty($_POST['heightto'])){{$_POST['heightto'] }}@elseif(!empty($_GET['heightto'])){{$_GET['heightto']}}@elseif(!empty(session()->get('search_page_key.heightto'))){{ session()->get('search_page_key.heightto')  }}@endif">
                                             {{--<input id="heightRange" name="heightRange" class="multi-range" type="range" />--}}
                                             </span>
                                         </dt>
@@ -1494,12 +1494,20 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
     </script>
     <script>
         $("#form").submit(function(e){
+            agefrom = $('#agefrom').val();
+            ageto = $('#ageto').val();
             heightfrom = $('#heightfrom').val();
             heightto = $('#heightto').val();
             weightfrom = $('#weightfrom').val();
             weightto = $('#weightto').val();
 
             //範圍須有值
+            if((agefrom == "" && ageto != "") || (agefrom != "" && ageto == ""))
+            {
+                event.preventDefault();
+                c5('請輸入年齡範圍');
+                return false;
+            }
             if((heightfrom == "" && heightto != "") || (heightfrom != "" && heightto == ""))
             {
                 event.preventDefault();
@@ -1515,6 +1523,30 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
             //範圍須有值
 
             //數值限制
+            if(agefrom < 18 || ageto < 18)
+            {
+                hint_str = "年齡範圍值必須大於或等於18。";
+                c5(hint_str);
+                return false;
+            }
+            if(agefrom > 80 || ageto > 80)
+            {
+                hint_str = "年齡範圍值必須小於或等於80。";
+                c5(hint_str);
+                return false;
+            }
+            if(heightfrom < 140 || heightto < 140)
+            {
+                hint_str = "身高範圍值必須大於或等於140。";
+                c5(hint_str);
+                return false;
+            }
+            if(heightfrom > 210 || heightto > 210)
+            {
+                hint_str = "身高範圍值必須小於或等於210。";
+                c5(hint_str);
+                return false;
+            }
             if(weightfrom % 5 != 0)
             {
                 hint_str = "體重範圍請輸入有效值。最接近的兩個有效值分別是" + (Math.floor((parseInt(weightfrom) / 5)) * 5) + "和" + (Math.floor((parseInt(weightfrom) / 5 + 1)) * 5) + "。";
@@ -1530,6 +1562,11 @@ header("Expires: Fri, 01 Jan 1990 00:00:00 GMT");
             //數值限制
 
             //大小交換
+            if(agefrom > ageto)
+            {
+                $('#agefrom').val(ageto);
+                $('#ageto').val(agefrom);
+            }
             if(heightfrom > heightto)
             {
                 $('#heightfrom').val(heightto);
