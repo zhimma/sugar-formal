d4057c15505a7ecb474b0e79125ec24cb3590adb
simon821010 <simon821010@gmail.com>
2023-09-22 10:01:14 +0800

    修正真心話數量

--- app/Console/Commands/LineNotifyRecommendList.php
+++ app/Console/Commands/LineNotifyRecommendList.php
@@ -75,7 +75,7 @@ class LineNotifyRecommendList extends Command
         {
             $message = "\n";
             $message .= "推薦人氣甜心: ";
-            $message .= $sweetheart->user->name . "(真心話數:" . $sweetheart->user->received_messages_count . ")" . "\n";
+            $message .= $sweetheart->user->name . "(真心話數:" . $sweetheart->truth_message_count . ")" . "\n";
             $message .= route("viewuser", ['uid' => $sweetheart->user->id]) . "\n";
             $picurl = url('/') . ($sweetheart->user->meta->pic ? (file_exists(public_path() . $sweetheart->user->meta->pic) ? $sweetheart->user->meta->pic : '/new/images/female.png') : '/new/images/female.png');
             
